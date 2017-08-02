356ab7eba4c203fb88ee86fc2e708dc85264b408
Rob Scanlon <robscanlon@gmail.com>
2017-08-02 10:23:51 -0400

    Allow patient/*.* scopes.

--- app.rb
+++ app.rb
@@ -158,7 +158,7 @@ stream :keep_open do |out|
     end
 
     # Parse accessible resources from scopes
-    accessible_resource_names = scopes.scan(/patient\/(.*?)\.read/)
+    accessible_resource_names = scopes.scan(/patient\/(.*?)\.[read|\*]/)
     accessible_resources = []
     if accessible_resource_names.include?(["*"])
       accessible_resources = supporting_resources.dup
