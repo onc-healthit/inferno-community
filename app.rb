8e7b33f96ff1b6e3dae3367055ffd0e8917562a1
Jason Walonoski <jwalonoski@mitre.org>
2017-11-15 16:07:40 -0500

    Fix crash when patient demographics are unavailable 403.

--- app.rb
+++ app.rb
@@ -184,38 +184,48 @@ stream :keep_open do |out|
     }.map {|n| n['type']}
 
     # Get the patient demographics
-    patient = client.read(Object.const_get("#{klass_header}Patient"), patient_id).resource
-    response.assert('Patient Successfully Retrieved',patient.is_a?(Object.const_get("#{klass_header}Patient")),patient.id)
-    patient_details = patient.to_hash
-    puts "Patient: #{patient_details['id']} #{patient_details['name']}"
+    patient_read_response = client.read(Object.const_get("#{klass_header}Patient"), patient_id)
+    patient = patient_read_response.resource
+    is_patient = patient.is_a?(Object.const_get("#{klass_header}Patient"))
+    response.assert('Patient Successfully Retrieved',is_patient,"HTTP #{patient_read_response.code} Patient/#{patient_id}")
+    if is_patient
+      patient_details = patient.to_hash
+      puts "Patient: #{patient_details['id']} #{patient_details['name']}"
 
-    # DAF/US-Core CCDS
-    response.assert('Patient Name',patient_details['name'],patient_details['name'])
-    response.assert('Patient Gender',patient_details['gender'],patient_details['gender'])
-    response.assert('Patient Date of Birth',patient_details['birthDate'],patient_details['birthDate'])
-    # US Extensions
-    puts 'Examining Patient for US-Core Extensions'
-    extensions = {
-      'Race' => 'http://hl7.org/fhir/StructureDefinition/us-core-race',
-      'Ethnicity' => 'http://hl7.org/fhir/StructureDefinition/us-core-ethnicity',
-      'Religion' => 'http://hl7.org/fhir/StructureDefinition/us-core-religion',
-      'Mother\'s Maiden Name' => 'http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName',
-      'Birth Place' => 'http://hl7.org/fhir/StructureDefinition/birthPlace'
-    }
-    required_extensions = ['Race','Ethnicity']
-    extensions.each do |name,url|
-      detail = nil
-      check = :not_found
-      if patient_details['extension']
-        detail = patient_details['extension'].find{|e| e['url']==url }
-        check = !detail.nil? if required_extensions.include?(name)
-      elsif required_extensions.include?(name)
-        check = false
+      # DAF/US-Core CCDS
+      response.assert('Patient Name',patient_details['name'],patient_details['name'])
+      response.assert('Patient Gender',patient_details['gender'],patient_details['gender'])
+      response.assert('Patient Date of Birth',patient_details['birthDate'],patient_details['birthDate'])
+      # US Extensions
+      puts 'Examining Patient for US-Core Extensions'
+      extensions = {
+        'Race' => 'http://hl7.org/fhir/StructureDefinition/us-core-race',
+        'Ethnicity' => 'http://hl7.org/fhir/StructureDefinition/us-core-ethnicity',
+        'Religion' => 'http://hl7.org/fhir/StructureDefinition/us-core-religion',
+        'Mother\'s Maiden Name' => 'http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName',
+        'Birth Place' => 'http://hl7.org/fhir/StructureDefinition/birthPlace'
+      }
+      required_extensions = ['Race','Ethnicity']
+      extensions.each do |name,url|
+        detail = nil
+        check = :not_found
+        if patient_details['extension']
+          detail = patient_details['extension'].find{|e| e['url']==url }
+          check = !detail.nil? if required_extensions.include?(name)
+        elsif required_extensions.include?(name)
+          check = false
+        end
+        response.assert("Patient #{name}", check, detail)
       end
-      response.assert("Patient #{name}", check, detail)
+      response.assert('Patient Preferred Language',(patient_details['communication'] && patient_details['communication'].find{|c|c['language'] && c['preferred']}),patient_details['communication'])
+    else
+      response.assert('Patient Name',:skip,'Unable to access patient demographics.')
+      response.assert('Patient Gender',:skip,'Unable to access patient demographics.')
+      response.assert('Patient Date of Birth',:skip,'Unable to access patient demographics.')
+      response.assert('Patient Race',:skip,'Unable to access patient demographics.')
+      response.assert('Patient Ethnicity',:skip,'Unable to access patient demographics.')
+      response.assert('Patient Preferred Language',:skip,'Unable to access patient demographics.')
     end
-    response.assert('Patient Preferred Language',(patient_details['communication'] && patient_details['communication'].find{|c|c['language'] && c['preferred']}),patient_details['communication'])
-
     # Get the patient's smoking status
     # {"coding":[{"system":"http://loinc.org","code":"72166-2"}]}
     puts 'Getting Smoking Status'
