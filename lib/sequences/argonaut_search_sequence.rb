d658d0f2dce2ea35216abe30878f852d3a41e446
Mahalingam <kmahalingam@mitre.org>
2018-01-05 16:29:09 -0500

    Implement Argonaut search test skeletons

---
+++ lib/sequences/argonaut_search_sequence.rb
@@ -0,0 +1,131 @@
+class ArgonautSearchSequence < SequenceBase
+
+  description 'The FHIR server properly follows the Argonaut Data Query Implementation Guide Server.'
+
+  # --------------------------------------------------
+  # Patient Search
+  # --------------------------------------------------
+
+  test 'Patient search' do
+
+    throw "TODO"
+  end
+
+  # --------------------------------------------------
+  # AllergyIntolerance Search
+  # --------------------------------------------------
+
+  test 'AllergyIntolerance search' do
+
+    throw "TODO"
+  end
+
+  # --------------------------------------------------
+  # CarePlan Search
+  # --------------------------------------------------
+
+  test 'CarePlan search' do
+
+    throw "TODO"
+  end
+
+  # --------------------------------------------------
+  # CareTeam Search
+  # --------------------------------------------------
+
+  test 'CareTeam search' do
+
+    throw "TODO"
+  end
+
+  # --------------------------------------------------
+  # Condition Search
+  # --------------------------------------------------
+
+  test 'Condition search' do
+
+    throw "TODO"
+  end
+
+  # --------------------------------------------------
+  # Device Search
+  # --------------------------------------------------
+
+  test 'Device search' do
+
+    throw "TODO"
+  end
+
+  # --------------------------------------------------
+  # Goal Search
+  # --------------------------------------------------
+
+  test 'Goal search' do
+
+    throw "TODO"
+  end
+
+  # --------------------------------------------------
+  # Immunization Search
+  # --------------------------------------------------
+
+  test 'Immunization search' do
+
+    throw "TODO"
+  end
+
+  # --------------------------------------------------
+  # DiagnosticReport Search
+  # --------------------------------------------------
+
+  test 'DiagnosticReport search' do
+
+    throw "TODO"
+  end
+
+  # --------------------------------------------------
+  # Medication Search
+  # --------------------------------------------------
+
+  test 'Medication search' do
+
+    throw "TODO"
+  end
+
+  # --------------------------------------------------
+  # MedicationStatement Search
+  # --------------------------------------------------
+
+  test 'MedicationStatement search' do
+
+    throw "TODO"
+  end
+
+  # --------------------------------------------------
+  # MedicationOrder Search
+  # --------------------------------------------------
+
+  test 'MedicationOrder search' do
+
+    throw "TODO"
+  end
+
+  # --------------------------------------------------
+  # Observation Search
+  # --------------------------------------------------
+
+  test 'Observation search' do
+
+    throw "TODO"
+  end
+
+  # --------------------------------------------------
+  # Procedure Search
+  # --------------------------------------------------
+
+  test 'Procedure search' do
+
+    throw "TODO"
+  end
+
+end
