771359d7436bdf770bf5160ed48d34223d0bac82
Jason Walonoski <jwalonoski@mitre.org>
2018-01-05 15:06:43 -0500

    Provider and Patient launch sequences.

--- lib/sequences/launch_sequence.rb
+++ lib/sequences/launch_sequence.rb
@@ -5,10 +5,7 @@ class LaunchSequence < SequenceBase
 
   test 'at least one successful launch' do
     results = @instance.latest_results
-    assert (results.include?('ProviderStandaloneLaunch') && results['ProviderStandaloneLaunch'].result=='pass') ||
-      (results.include?('ProviderEHRLaunch') && results['ProviderEHRLaunch'].result=='pass') ||
-      (results.include?('PatientStandaloneLaunch') && results['PatientStandaloneLaunch'].result=='pass') ||
-      (results.include?('PatientEHRLaunch') && results['PatientEHRLaunch'].result=='pass')
+    assert (results.include?('ProviderEHRLaunch') && results['ProviderEHRLaunch'].result=='pass') ||
+      (results.include?('PatientStandaloneLaunch') && results['PatientStandaloneLaunch'].result=='pass')
   end
-
 end
