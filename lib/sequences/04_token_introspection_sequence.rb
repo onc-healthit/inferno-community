17c10f5ab0cf98e01f2d40410af473e63e300df1
Justin Richer <jricher@mit.edu>
2018-01-24 16:22:38 -0500

    added additional introspection tests for token active state and scopes

--- lib/sequences/04_token_introspection_sequence.rb
+++ lib/sequences/04_token_introspection_sequence.rb
@@ -29,8 +29,33 @@ class TokenIntrospectionSequence < SequenceBase
 
     assert !(@introspection_response['error'] || @introspection_response['error_description']), 'Got an error from the introspection endpoint'
 
-
   end
 
+  test 'Access token is active',
+          'https://tools.ietf.org/html/rfc7662',
+          'A current access token is listed as "active"' do
+    
+    active = @introspection_response['active']
+    
+    assert active, 'Token is not active, try the test again with a valid token'
+  end
+
+  test 'Scopes match',
+          'https://tools.ietf.org/html/rfc7662',
+          'The scopes we received alongside the token match those from the introspection response' do
+
+    expected_scopes = @instance.scopes.split(' ')
+    actual_scopes = @introspection_response['scope'].split(' ')
+    
+    FHIR.logger.debug "Introspection: Expected scopes #{expected_scopes}, Actual scopes #{actual_scopes}"
+    
+    warning {
+      missing_scopes = (expected_scopes - actual_scopes)
+      assert missing_scopes.empty?, "Introspection response did not include expected scopes: #{missing_scopes}"
+    }
+    extra_scopes = (actual_scopes - expected_scopes)
+    assert extra_scopes.empty?, "Introspection response included additional scopes: #{extra_scopes}"
+    
+  end 
 
 end
