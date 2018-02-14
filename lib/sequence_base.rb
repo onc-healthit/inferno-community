eb1ddcbdd1eed4110c7f496b739dc920686c7cc3
Rob Scanlon <robscanlon@gmail.com>
2018-02-13 22:52:08 -0500

    Add placeholders for read, history and vread and rename search sequence to data query sequence.

--- lib/sequence_base.rb
+++ lib/sequence_base.rb
@@ -287,7 +287,7 @@ class SequenceBase
         ProviderEHRLaunchSequence, 
         TokenIntrospectionSequence, 
         ArgonautProfilesSequence, 
-        ArgonautSearchSequence,
+        ArgonautDataQuerySequence,
         AdditionalResourcesSequence]
   end
 
