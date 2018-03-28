f6f0208ff2edfe912f1d4d883c15a6788e5751ff
Mahalingam <kmahalingam@mitre.org>
2018-03-28 15:33:25 -0400

    Implement token introspection self testing

--- lib/sequences/06_token_introspection_sequence.rb
+++ lib/sequences/06_token_introspection_sequence.rb
@@ -1,32 +1,35 @@
 class TokenIntrospectionSequence < SequenceBase
- 
+
   title 'OAuth 2.0 Token Introspection'
 
   description 'Verify token properties using token introspection at the authorization server'
 
   optional
-  
+
   modal_before_run
 
-  preconditions 'Client must be authorized.' do 
+  preconditions 'Client must be authorized.' do
     !@instance.token.nil?
   end
 
   test 'Call token introspection endpoint',
           'https://tools.ietf.org/html/rfc7662',
           'A resource server is capable of calling the introspection endpoint' do
-    
+
     headers = { 'Accept' => 'application/json', 'Content-type' => 'application/x-www-form-urlencoded' }
-    
+
     params = {
       'token' => @instance.introspect_token,
       'client_id' => @instance.resource_id,
       'client_secret' => @instance.resource_secret
     }
-    
+
     @introspection_response = LoggedRestClient.post(@instance.oauth_introspection_endpoint, params, headers)
-    @introspection_response = JSON.parse(@introspection_response.body)
-    
+    assert !@introspection_response.nil?, 'No introspection response'
+    assert_response_ok(@introspection_response)
+    @introspection_response_body = JSON.parse(@introspection_response.body)
+    assert !@introspection_response_body.nil?, 'No introspection response body'
+
     FHIR.logger.debug "Introspection response: #{@introspection_response}"
 
     assert !(@introspection_response['error'] || @introspection_response['error_description']), 'Got an error from the introspection endpoint'
@@ -36,9 +39,10 @@ class TokenIntrospectionSequence < SequenceBase
   test 'Access token is active',
           'https://tools.ietf.org/html/rfc7662',
           'A current access token is listed as "active"' do
-    
-    active = @introspection_response['active']
-    
+
+    assert !@introspection_response_body.nil?, 'No introspection response body'
+
+    active = @introspection_response_body['active']
     assert active, 'Token is not active, try the test again with a valid token'
   end
 
@@ -47,24 +51,28 @@ class TokenIntrospectionSequence < SequenceBase
           'The scopes we received alongside the token match those from the introspection response',
           :optional do
 
+    assert !@introspection_response_body.nil?, 'No introspection response body'
+
     expected_scopes = @instance.scopes.split(' ')
-    actual_scopes = @introspection_response['scope'].split(' ')
-    
+    actual_scopes = @introspection_response_body['scope'].split(' ')
+
     FHIR.logger.debug "Introspection: Expected scopes #{expected_scopes}, Actual scopes #{actual_scopes}"
-    
+
     missing_scopes = (expected_scopes - actual_scopes)
     assert missing_scopes.empty?, "Introspection response did not include expected scopes: #{missing_scopes}"
     extra_scopes = (actual_scopes - expected_scopes)
 
     assert extra_scopes.empty?, "Introspection response included additional scopes: #{extra_scopes}"
-    
+
   end
-  
+
   test 'Token expiration',
           '',
           'The token should have a lifetime of at least 60 minutes' do
-  
-    expiration = Time.at(@introspection_response['exp']).to_datetime
+
+    assert !@introspection_response_body.nil?, 'No introspection response body'
+
+    expiration = Time.at(@introspection_response_body['exp']).to_datetime
     token_retrieved_at = @instance.token_retrieved_at
     now = DateTime.now
 
@@ -72,9 +80,9 @@ class TokenIntrospectionSequence < SequenceBase
     clock_slip = 5 # a few seconds of clock skew allowed
 
     assert (expiration - token_retrieved_at) < max_token_seconds, "Token does not have adequate lifetime of at least #{max_token_seconds} seconds"
-    
+
     assert (now + Rational(clock_slip, (24 * 60 * 60))) < expiration, "Token has expired"
-            
+
   end
 
 end
---
+++ test/sequence/token_introspection_test.rb
@@ -0,0 +1,192 @@
+require File.expand_path '../../test_helper.rb', __FILE__
+
+class TokenIntrospectionSequenceTest < MiniTest::Unit::TestCase
+
+  REQUEST_HEADERS = { 'Accept' => 'application/json', 'Content-type' => 'application/x-www-form-urlencoded' }
+
+  def setup
+
+    introspect_token = JSON::JWT.new({iss: 'foo'})
+    resource_id = SecureRandom.uuid
+    resource_secret = SecureRandom.hex(32)
+
+    @instance = TestingInstance.new(url: 'http://www.example.com',
+                                   client_name: 'Crucible Smart App',
+                                   base_url: 'http://localhost:4567',
+                                   scopes: 'launch openid patient/*.* profile',
+                                   oauth_introspection_endpoint: 'https://oauth_reg.example.com/introspect',
+                                   introspect_token: introspect_token,
+                                   resource_id: resource_id,
+                                   resource_secret: resource_secret,
+                                   token_retrieved_at: DateTime.now
+                                   )
+    @instance.save! # this is for convenience.  we could rewrite to ensure nothing gets saved within tests.
+    client = FHIR::Client.new(@instance.url)
+    client.use_dstu2
+    client.default_json
+    @sequence = TokenIntrospectionSequence.new(@instance, client)
+  end
+
+  def test_all_pass
+    WebMock.reset!
+    params = {
+      'token' => @instance.introspect_token,
+      'client_id' => @instance.resource_id,
+      'client_secret' => @instance.resource_secret
+    }
+    response = {
+      "active" => true,
+      "scope" => @instance.scopes,
+      "exp" => 2.hours.from_now.to_i
+    }
+
+    stub_register = stub_request(:post, @instance.oauth_introspection_endpoint).
+      with(headers: REQUEST_HEADERS, body: params).
+      to_return(status: 200, body: response.to_json)
+
+    sequence_result = @sequence.start
+
+    assert_requested(stub_register)
+
+    failures = sequence_result.test_results.select{|r| r.result != 'pass'}
+
+    assert failures.length == 0, "All tests should pass.  First error: #{!failures.empty? && failures.first.message}"
+    assert sequence_result.result == 'pass', 'Sequence should pass.'
+    assert sequence_result.test_results.all?{|r| r.test_warnings.empty? }, 'There should not be any warnings.'
+  end
+
+  def test_no_introspection_endpoint
+    WebMock.reset!
+    params = {
+      'token' => @instance.introspect_token,
+      'client_id' => @instance.resource_id,
+      'client_secret' => @instance.resource_secret
+    }
+
+    stub_register = stub_request(:post, @instance.oauth_introspection_endpoint).
+      with(headers: REQUEST_HEADERS, body: params).
+      to_return(status: 404)
+
+    sequence_result = @sequence.start
+
+    assert_requested(stub_register)
+
+    assert sequence_result.result == 'fail', 'Sequence should fail.'
+    assert sequence_result.test_results.all?{|r| r.result == 'fail' }, 'All tests should fail.'
+  end
+
+  def test_inactive
+    WebMock.reset!
+    params = {
+      'token' => @instance.introspect_token,
+      'client_id' => @instance.resource_id,
+      'client_secret' => @instance.resource_secret
+    }
+    response = {
+      "active" => false,
+      "scope" => @instance.scopes,
+      "exp" => 2.hours.from_now.to_i
+    }
+
+    stub_register = stub_request(:post, @instance.oauth_introspection_endpoint).
+      with(headers: REQUEST_HEADERS, body: params).
+      to_return(status: 200, body: response.to_json)
+
+    sequence_result = @sequence.start
+
+    assert_requested(stub_register)
+
+    failures = sequence_result.test_results.select{|r| r.result != 'pass'}
+
+    # 1 test depends on active being true
+    assert failures.length == 1, 'One test should fail.'
+    assert sequence_result.result == 'fail', 'Sequence should fail.'
+  end
+
+  def test_insufficient_scopes
+    WebMock.reset!
+    params = {
+      'token' => @instance.introspect_token,
+      'client_id' => @instance.resource_id,
+      'client_secret' => @instance.resource_secret
+    }
+    response = {
+      "active" => true,
+      "scope" => @instance.scopes.split(' ')[0...-1].join(' '), # remove last scope
+      "exp" => 2.hours.from_now.to_i
+    }
+
+    stub_register = stub_request(:post, @instance.oauth_introspection_endpoint).
+      with(headers: REQUEST_HEADERS, body: params).
+      to_return(status: 200, body: response.to_json)
+
+    sequence_result = @sequence.start
+
+    assert_requested(stub_register)
+
+    failures = sequence_result.test_results.select{|r| r.result != 'pass'}
+    warnings = sequence_result.test_results.select{|r| !r.test_warnings.empty?}
+
+    # 1 optional test depends on correct scopes
+    assert failures.length == 1, "One test should fail."
+    assert sequence_result.result == 'pass', 'Sequence should pass.'
+  end
+
+  def test_additional_scopes
+    WebMock.reset!
+    params = {
+      'token' => @instance.introspect_token,
+      'client_id' => @instance.resource_id,
+      'client_secret' => @instance.resource_secret
+    }
+    response = {
+      "active" => true,
+      "scope" => @instance.scopes + ' extra', # add extra scope
+      "exp" => 2.hours.from_now.to_i
+    }
+
+    stub_register = stub_request(:post, @instance.oauth_introspection_endpoint).
+      with(headers: REQUEST_HEADERS, body: params).
+      to_return(status: 200, body: response.to_json)
+
+    sequence_result = @sequence.start
+
+    assert_requested(stub_register)
+
+    failures = sequence_result.test_results.select{|r| r.result != 'pass'}
+    warnings = sequence_result.test_results.select{|r| !r.test_warnings.empty?}
+
+    # 1 optional test depends on correct scopes
+    assert failures.length == 1, "One test should fail."
+    assert sequence_result.result == 'pass', 'Sequence should pass.'
+  end
+
+  def test_expiration
+    WebMock.reset!
+    params = {
+      'token' => @instance.introspect_token,
+      'client_id' => @instance.resource_id,
+      'client_secret' => @instance.resource_secret
+    }
+    response = {
+      "active" => false,
+      "scope" => @instance.scopes,
+      "exp" => 30.minutes.from_now.to_i # should be at least 60 minutes
+    }
+
+    stub_register = stub_request(:post, @instance.oauth_introspection_endpoint).
+      with(headers: REQUEST_HEADERS, body: params).
+      to_return(status: 200, body: response.to_json)
+
+    sequence_result = @sequence.start
+
+    assert_requested(stub_register)
+
+    failures = sequence_result.test_results.select{|r| r.result != 'pass'}
+
+    # 1 test depends on expiration being at least 60 minutes
+    assert failures.length == 1, 'One test should fail.'
+    assert sequence_result.result == 'fail', 'Sequence should fail.'
+  end
+
+end
