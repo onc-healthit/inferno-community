be778d9be7360f81a917cb59b61d20a558c2f4b2
Reece Adamson <radamson@mitre.org>
2018-10-18 16:24:49 -0400

    add method to support_resource to return supported interactions

--- lib/app/models/supported_resource.rb
+++ lib/app/models/supported_resource.rb
@@ -13,6 +13,22 @@ module Inferno
       property :scope_supported, Boolean, default: false
 
       belongs_to :testing_instance
+
+
+      # Returns an array containing the supported interaction of the resource
+      #
+      # @return [Array<Symbol>, nil] the supported interactions
+      #   Returns nil if resource is not supported
+      def supported_interactions
+        return nil unless supported
+        interactions = []
+        interactions << :read if read_supported
+        interactions << :vread if vread_supported
+        interactions << :search if search_supported
+        interactions << :history if history_supported
+        interactions << :authorized if scope_supported
+        interactions
+      end
     end
   end
 end
