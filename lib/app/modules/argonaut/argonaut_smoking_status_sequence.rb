9b46ec7464104df2338921197c89b8df22ebd2c4
unknown <chasezhou16@gmail.com>
2019-02-28 20:59:20 -0500

    smoking status uses subject

--- lib/app/modules/argonaut/argonaut_smoking_status_sequence.rb
+++ lib/app/modules/argonaut/argonaut_smoking_status_sequence.rb
@@ -16,7 +16,7 @@ module Inferno
       def validate_resource_item (resource, property, value)
         case property
         when "patient"
-          assert (resource.patient && resource.patient.reference.include?(value)), "Patient on resource does not match patient requested"
+          assert (resource.subject && resource.subject.reference.include?(value)), "Patient on resource does not match patient requested"
         when "code"
           code = resource.try(:code).try(:coding).try(:first).try(:code)
           assert !code.nil? && code == value, "Code on resource did not match code requested"
