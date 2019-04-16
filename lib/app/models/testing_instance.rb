ceebd5324250009ff81c802db88fbdc679becb54
unknown <chasezhou16@gmail.com>
2019-04-16 00:55:36 -0400

    moving some properties and functions out of the instance and sequence_base

--- lib/app/models/testing_instance.rb
+++ lib/app/models/testing_instance.rb
@@ -19,26 +19,16 @@ module Inferno
       property :state, String
       property :selected_module, String
 
-      property :conformance_checked, Boolean
       property :oauth_authorize_endpoint, String
       property :oauth_token_endpoint, String
       property :oauth_register_endpoint, String
-      property :fhir_format, String
 
-      property :dynamically_registered, Boolean
       property :client_endpoint_key, String, default: proc { Inferno::SecureRandomBase62.generate(32) }
 
       property :token, String
-      property :token_retrieved_at, DateTime
       property :id_token, String
       property :refresh_token, String
       property :created_at, DateTime, default: proc { DateTime.now }
-
-      property :oauth_introspection_endpoint, String
-      property :resource_id, String
-      property :resource_secret, String
-      property :introspect_token, String
-      property :introspect_refresh_token, String
 
       property :standalone_launch_script, String
       property :ehr_launch_script, String
@@ -52,10 +42,6 @@ module Inferno
       has n, :sequence_results
       has n, :supported_resources, order: [:index.asc]
       has n, :resource_references
-
-      def self.add_property(name, type)
-        property name, type
-      end
 
       def latest_results
         self.sequence_results.reduce({}) do |hash, result|
@@ -130,113 +116,8 @@ module Inferno
 
       end
 
-      def fhir_version
-        self.module.fhir_version
-      end
-
       def module
         Inferno::Module.get(self.selected_module)
-      end
-
-      def patient_id
-        self.resource_references.select{|ref| ref.resource_type == 'Patient'}.first.try(:resource_id)
-      end
-
-      def patient_id= patient_id
-        return if patient_id.to_s == self.patient_id.to_s
-
-        existing_patients = self.resource_references.select{|ref| ref.resource_type == 'Patient'}
-        # Use destroy directly (instead of on each, so we don't have to reload)
-        self.resource_references.destroy
-        self.save!
-
-        self.resource_references << ResourceReference.new({
-                                                              resource_type: 'Patient',
-                                                              resource_id: patient_id
-                                                          })
-
-      end
-
-      def save_supported_resources(conformance)
-
-        resources = ['Patient',
-                     'AllergyIntolerance',
-                     'CarePlan',
-                     'Condition',
-                     'Device',
-                     'DiagnosticReport',
-                     'DocumentReference',
-                     'ExplanationOfBenefit',
-                     'Goal',
-                     'Immunization',
-                     'Medication',
-                     'MedicationDispense',
-                     'MedicationStatement',
-                     'MedicationOrder',
-                     'Observation',
-                     'Procedure',
-                     'DocumentReference',
-                     'Provenance']
-
-        supported_resources = conformance.rest.first.resource.select{ |r| resources.include? r.type}.reduce({}){|a,k| a[k.type] = k; a}
-
-        self.supported_resources.each(&:destroy)
-        self.save!
-
-        resources.each_with_index do |resource_name, index|
-
-          resource = supported_resources[resource_name]
-
-          read_supported = resource && resource.interaction && resource.interaction.any?{|i| i.code == 'read'}
-
-          self.supported_resources << SupportedResource.create({
-                                                                   resource_type: resource_name,
-                                                                   index: index,
-                                                                   testing_instance_id: self.id,
-                                                                   supported: !resource.nil?,
-                                                                   read_supported: read_supported,
-                                                                   vread_supported: resource && resource.interaction && resource.interaction.any?{|i| i.code == 'vread'},
-                                                                   search_supported: resource && resource.interaction && resource.interaction.any?{|i| i.code == 'search-type'},
-                                                                   history_supported: resource && resource.interaction && resource.interaction.any?{|i| i.code == 'history-instance'}
-                                                               })
-        end
-
-        self.save!
-
-      end
-
-      def conformance_supported?(resource, methods = [])
-
-        resource_support = self.supported_resources.find {|r| r.resource_type == resource.to_s}
-        return false if resource_support.nil? || !resource_support.supported
-
-        methods.all? do |method|
-          case method
-          when :read
-            resource_support.read_supported
-          when :search
-            resource_support.search_supported
-          when :history
-            resource_support.history_supported
-          when :vread
-            resource_support.vread_supported
-          else
-            false
-          end
-        end
-      end
-
-      def post_resource_references(resource_type: nil, resource_id: nil)
-        self.resource_references.each do |ref|
-          if (ref.resource_type == resource_type) && (ref.resource_id == resource_id)
-            ref.destroy
-          end
-        end
-        self.resource_references << ResourceReference.new({resource_type: resource_type,
-                                                          resource_id: resource_id})
-        self.save!
-        # Ensure the instance resource references are accurate
-        self.reload
       end
     end
   end
