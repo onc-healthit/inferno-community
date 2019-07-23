d40949597ad6581877ea2644db3a946c4525646c
Chase Zhou <czhou@mitre.org>
2019-07-23 17:44:39 -0400

    Changes to range comparer and adding unit tests

--- lib/app/sequence_base.rb
+++ lib/app/sequence_base.rb
@@ -732,6 +732,55 @@ module Inferno
         end
         assert(errors.empty?, errors.join("<br/>\n"))
       end
+
+      def can_resolve_path(element, path)
+        if path.empty?
+          return false if element.nil?
+
+          return Array.wrap(element).any? { |el| yield(el) } if block_given?
+
+          return true
+        end
+
+        path_ary = path.split('.')
+        el_as_array = Array.wrap(element)
+        cur_path_part = path_ary.shift.to_sym
+        return false if el_as_array.none? { |el| el.try(cur_path_part).present? }
+
+        if block_given?
+          el_as_array.any? { |el| can_resolve_path(el.send(cur_path_part), path_ary.join('.')) { |value_found| yield(value_found) } }
+        else
+          el_as_array.any? { |el| can_resolve_path(el.send(cur_path_part), path_ary.join('.')) }
+        end
+      end
+
+      def resolve_element_from_path(element, path)
+        el_as_array = Array.wrap(element)
+        return el_as_array&.first if path.empty?
+
+        path_ary = path.split('.')
+        cur_path_part = path_ary.shift.to_sym
+
+        found_subset = el_as_array.select { |el| el.try(cur_path_part).present? }
+        return nil if found_subset.empty?
+
+        found_subset.each do |el|
+          el_found = resolve_element_from_path(el.send(cur_path_part), path_ary.join('.'))
+          return el_found unless el_found.nil?
+        end
+        nil
+      end
+
+      def date_comparator_value(comparator, date)
+        case comparator
+        when 'lt', 'le'
+          comparator + (DateTime.xmlschema(date) + 1).xmlschema
+        when 'gt', 'ge'
+          comparator + (DateTime.xmlschema(date) - 1).xmlschema
+        else
+          ''
+        end
+      end
     end
 
     Dir.glob(File.join(__dir__, 'modules', '**', '*_sequence.rb')).each { |file| require file }
--- lib/app/utils/search_validation.rb
+++ lib/app/utils/search_validation.rb
@@ -2,64 +2,15 @@
 
 module Inferno
   module SearchValidationUtil
-    def can_resolve_path(element, path)
-      if path.empty?
-        return false if element.nil?
-
-        return Array.wrap(element).any? { |el| yield(el) } if block_given?
-
-        return true
-      end
-
-      path_ary = path.split('.')
-      el_as_array = Array.wrap(element)
-      cur_path_part = path_ary.shift.to_sym
-      return false if el_as_array.none? { |el| el.try(cur_path_part).present? }
-
-      if block_given?
-        el_as_array.any? { |el| can_resolve_path(el.send(cur_path_part), path_ary.join('.')) { |value_found| yield(value_found) } }
-      else
-        el_as_array.any? { |el| can_resolve_path(el.send(cur_path_part), path_ary.join('.')) }
-      end
-    end
-
-    def resolve_element_from_path(element, path)
-      el_as_array = Array.wrap(element)
-      return el_as_array&.first if path.empty?
-
-      path_ary = path.split('.')
-      cur_path_part = path_ary.shift.to_sym
-
-      found_subset = el_as_array.select { |el| el.try(cur_path_part).present? }
-      return nil if found_subset.empty?
-
-      found_subset.each do |el|
-        el_found = resolve_element_from_path(el.send(cur_path_part), path_ary.join('.'))
-        return el_found unless el_found.nil?
-      end
-      nil
-    end
-
-    def date_comparator_value(comparator, date)
-      case comparator
-      when 'lt', 'le'
-        comparator + (DateTime.xmlschema(date) + 1).xmlschema
-      when 'gt', 'ge'
-        comparator + (DateTime.xmlschema(date) - 1).xmlschema
-      else
-        ''
-      end
-    end
-
     def get_fhir_datetime_range(datetime)
       range = { start: DateTime.xmlschema(datetime), end: nil }
       range[:end] =
         if /^\d{4}$/.match?(datetime) # YYYY
-          range[:start].next_year
+          range[:start].next_year - 1.seconds
         elsif /^\d{4}-\d{2}$/.match?(datetime) # YYYY-MM
-          range[:start].next_month
-        elsif /^\d{4}-\d{2}-\d{2}/.match?(datetime) # YYYY-MM-DD
-          range[:start].next_day
+          range[:start].next_month - 1.seconds
+        elsif /^\d{4}-\d{2}-\d{2}$/.match?(datetime) # YYYY-MM-DD
+          range[:start].next_day - 1.seconds
         else # YYYY-MM-DDThh:mm:ss+zz:zz
           range[:start]
         end
@@ -74,11 +25,11 @@ module Inferno
       period_end_beginning = DateTime.xmlschema(period.end)
       range[:end] =
         if /^\d{4}$/.match?(period.end) # YYYY
-          period_end_beginning.next_year
+          period_end_beginning.next_year - 1.seconds
         elsif /^\d{4}-\d{2}$/.match?(period.end) # YYYY-MM
-          period_end_beginning.next_month
-        elsif /^\d{4}-\d{2}-\d{2}/.match?(period.end) # YYYY-MM-DD
-          period_end_beginning.next_day
+          period_end_beginning.next_month - 1.seconds
+        elsif /^\d{4}-\d{2}-\d{2}$/.match?(period.end) # YYYY-MM-DD
+          period_end_beginning.next_day - 1.seconds
         else # YYYY-MM-DDThh:mm:ss+zz:zz
           period_end_beginning
         end
@@ -91,24 +42,27 @@ module Inferno
       when 'eq' # the range of the search value fully contains the range of the target value
         !target_range[:start].nil? && !target_range[:end].nil? && search_range[:start] <= target_range[:start] && search_range[:end] >= target_range[:end]
       when 'ne' # the range of the search value does not fully contain the range of the target value
-        !target_range[:start].nil? || !target_range[:end].nil? || search_range[:start] > target_range[:start] || search_range[:end] < target_range[:end]
+        target_range[:start].nil? || target_range[:end].nil? || search_range[:start] > target_range[:start] || search_range[:end] < target_range[:end]
       when 'gt' #	the range above the search value intersects (i.e. overlaps) with the range of the target value
-        target_range[:end].nil? || search_range[:end] <= target_range[:end]
+        target_range[:end].nil? || search_range[:end] < target_range[:end]
       when 'lt' # the range below the search value intersects (i.e. overlaps) with the range of the target value
-        target_range[:start].nil? || search_range[:start] >= target_range[:start]
+        target_range[:start].nil? || search_range[:start] > target_range[:start]
       when 'ge'
         fhir_date_comparer(search_range, target_range, 'gt') || fhir_date_comparer(search_range, target_range, 'eq')
       when 'le'
         fhir_date_comparer(search_range, target_range, 'lt') || fhir_date_comparer(search_range, target_range, 'eq')
       when 'sa' # the range above the search value contains the range of the target value
-        target_range[:start].nil? || search_range[:end] < target_range[:start]
+        !target_range[:start].nil? && search_range[:end] < target_range[:start]
       when 'eb' # the range below the search value contains the range of the target value
-        target_range[:end].nil? || search_range[:start] > target_range[:end]
+        !target_range[:end].nil? && search_range[:start] > target_range[:end]
       when 'ap' # the range of the search value overlaps with the range of the target value
-        (target_range[:start].nil? && search_range[:start] <= target_range[:end]) ||
-          (target_range[:end].nil? && search_range[:end] >= target_range[:start]) ||
+        if target_range[:start].nil? || target_range[:end].nil?
+          (target_range[:start].nil? && search_range[:start] < target_range[:end]) ||
+            (target_range[:end].nil? && search_range[:end] > target_range[:start])
+        else
           (search_range[:start] >= target_range[:start] && search_range[:start] <= target_range[:end]) ||
-          (search_range[:end] >= target_range[:start] && search_range[:end] <= target_range[:end])
+            (search_range[:end] >= target_range[:start] && search_range[:end] <= target_range[:end])
+        end
       end
     end
 
