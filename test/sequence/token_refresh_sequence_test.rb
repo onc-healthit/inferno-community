d60c3aa6b553db6d672df62c2246338fb96f7926
Stephen MacVicar <smacvicar@mitre.org>
2019-07-23 13:47:18 -0400

    refactor token refresh tests

--- test/sequence/token_refresh_sequence_test.rb
+++ test/sequence/token_refresh_sequence_test.rb
@@ -7,18 +7,20 @@ require_relative '../test_helper'
 class TokenRefreshSequenceTest < MiniTest::Test
   def setup
     refresh_token = JSON::JWT.new(iss: 'foo_refresh')
-    @instance = Inferno::Models::TestingInstance.new(url: 'http://www.example.com',
-                                                     client_name: 'Inferno',
-                                                     base_url: 'http://localhost:4567',
-                                                     client_endpoint_key: Inferno::SecureRandomBase62.generate(32),
-                                                     client_id: SecureRandom.uuid,
-                                                     selected_module: 'argonaut',
-                                                     oauth_authorize_endpoint: 'http://oauth_reg.example.com/authorize',
-                                                     oauth_token_endpoint: 'http://oauth_reg.example.com/token',
-                                                     initiate_login_uri: 'http://localhost:4567/launch',
-                                                     redirect_uris: 'http://localhost:4567/redirect',
-                                                     scopes: 'launch openid patient/*.* profile',
-                                                     refresh_token: refresh_token)
+    @instance = Inferno::Models::TestingInstance.new(
+      url: 'http://www.example.com',
+      client_name: 'Inferno',
+      base_url: 'http://localhost:4567',
+      client_endpoint_key: Inferno::SecureRandomBase62.generate(32),
+      client_id: SecureRandom.uuid,
+      selected_module: 'argonaut',
+      oauth_authorize_endpoint: 'http://oauth_reg.example.com/authorize',
+      oauth_token_endpoint: 'http://oauth_reg.example.com/token',
+      initiate_login_uri: 'http://localhost:4567/launch',
+      redirect_uris: 'http://localhost:4567/redirect',
+      scopes: 'launch openid patient/*.* profile',
+      refresh_token: refresh_token
+    )
 
     @instance.save! # this is for convenience.  we could rewrite to ensure nothing gets saved within tests.
     client = FHIR::Client.new(@instance.url)
@@ -28,47 +30,41 @@ class TokenRefreshSequenceTest < MiniTest::Test
     @standalone_token_exchange = load_json_fixture(:standalone_token_exchange)
   end
 
-  def all_pass(confidential)
+  def setup_mocks
     WebMock.reset!
-    if confidential
-      # Responses Must Contain Authorization Header
-      stub_request(:post, @instance.oauth_token_endpoint)
-        .with(headers: { 'Content-Type' => 'application/x-www-form-urlencoded',
-                         'Authorization' =>
-                              "Basic #{Base64.strict_encode64(@instance.client_id +
-                                                                  ':' +
-                                                                  @instance.client_secret)}" },
-              body: { 'grant_type' => 'refresh_token',
-                      'refresh_token' => @instance.refresh_token })
-        .to_return(status: 200,
-                   body: @standalone_token_exchange.to_json,
-                   headers: { content_type: 'application/json; charset=UTF-8',
-                              cache_control: 'no-store',
-                              pragma: 'no-cache' })
 
-      # Responses must NOT contain client_id in the body or the client secret in any situation
-      stub_request(:post, @instance.oauth_token_endpoint)
-        .with(body: /client_id|client_secret/)
-        .to_return(status: 401)
+    headers = {
+      'Content-Type' => 'application/x-www-form-urlencoded',
+    }
+    body = {
+      'grant_type' => 'refresh_token',
+      'refresh_token' => @instance.refresh_token
+    }
+
+    if @instance.client_secret.present?
+      headers['Authorization'] = "Basic #{Base64.strict_encode64(@instance.client_id + ':' + @instance.client_secret)}"
     else
-      stub_request(:post, @instance.oauth_token_endpoint)
-        .with(headers: { 'Content-Type' => 'application/x-www-form-urlencoded' },
-              body: { 'client_id' => @instance.client_id,
-                      'grant_type' => 'refresh_token',
-                      'refresh_token' => @instance.refresh_token })
-        .to_return(status: 200,
-                   body: @standalone_token_exchange.to_json,
-                   headers: { content_type: 'application/json; charset=UTF-8',
-                              cache_control: 'no-store',
-                              pragma: 'no-cache' })
+      body['client_id'] = @instance.client_id
     end
+
+    stub_request(:post, @instance.oauth_token_endpoint)
+      .with(headers: headers,
+            body: body)
+      .to_return(status: 200,
+                 body: @standalone_token_exchange.to_json,
+                 headers: { content_type: 'application/json; charset=UTF-8',
+                            cache_control: 'no-store',
+                            pragma: 'no-cache' })
 
     # To test rejection of invalid client_id
     stub_request(:post, @instance.oauth_token_endpoint)
       .with(body: /INVALID/,
             headers: { 'Content-Type' => 'application/x-www-form-urlencoded' })
       .to_return(status: 401)
+  end
 
+  def all_pass
+    setup_mocks
     sequence_result = @sequence.start
 
     assert sequence_result.pass?, 'The sequence should be marked as pass.'
@@ -77,12 +73,12 @@ class TokenRefreshSequenceTest < MiniTest::Test
   def test_all_pass_confidential_client
     @instance.client_secret = SecureRandom.uuid
     @instance.confidential_client = true
-    all_pass(true)
+    all_pass
   end
 
   def test_all_pass_public_client
     @instance.client_secret = nil
     @instance.confidential_client = nil
-    all_pass(false)
+    all_pass
   end
 end
