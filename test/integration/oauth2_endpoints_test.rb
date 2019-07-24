a6b6b5d3e37e557975ff967bd11c0a40680c5e64
Stephen MacVicar <smacvicar@mitre.org>
2019-07-24 14:26:49 -0400

    add oauth2 redirect endpoint test

--- test/integration/oauth2_endpoints_test.rb
+++ test/integration/oauth2_endpoints_test.rb
@@ -21,14 +21,13 @@ class HomePageTest < MiniTest::Test
   end
 
   def test_launch_response_success
-    instance = Inferno::Models::TestingInstance.new(
+    instance = Inferno::Models::TestingInstance.create(
       url: 'http://example.com',
       client_endpoint_key: 'static',
       selected_module: 'smart',
-      initiate_login_uri: '/launch'
+      initiate_login_uri: '/login'
     )
-    instance.save
-    sr = Inferno::Models::SequenceResult.new(
+    sr = Inferno::Models::SequenceResult.create(
       result: 'wait',
       testing_instance: instance,
       test_set_id: 'developer',
@@ -37,14 +36,39 @@ class HomePageTest < MiniTest::Test
       next_test_cases: '',
       redirect_to_url: '/redirect'
     )
-    sr.save
-    tr = Inferno::Models::TestResult.new(
+    Inferno::Models::TestResult.create(
       sequence_result: sr
     )
-    tr.save
 
     EventMachine.run do
       get '/inferno/oauth2/static/launch?iss=http://example.com'
+      assert last_response.ok?
+      assert last_response.body.include? js_redirect("#{Inferno::BASE_PATH}/#{instance.id}/test_sets/#{sr.test_set_id}/#SMARTonFHIRTesting/#{sr.test_case_id}")
+      break
+    end
+  end
+
+  def test_redirect_response_success
+    instance = Inferno::Models::TestingInstance.create(
+      state: 'abc123',
+      client_endpoint_key: 'static',
+      selected_module: 'smart',
+      initiate_login_uri: '/login'
+    )
+    sr = Inferno::Models::SequenceResult.create(
+      result: 'wait',
+      testing_instance: instance,
+      test_set_id: 'developer',
+      test_case_id: 'developer_SMARTonFHIRTesting_EHRLaunchSequence',
+      wait_at_endpoint: 'redirect',
+      next_test_cases: ''
+    )
+    Inferno::Models::TestResult.create(
+      sequence_result: sr
+    )
+
+    EventMachine.run do
+      get '/inferno/oauth2/static/redirect?state=abc123'
       assert last_response.ok?
       assert last_response.body.include? js_redirect("#{Inferno::BASE_PATH}/#{instance.id}/test_sets/#{sr.test_set_id}/#SMARTonFHIRTesting/#{sr.test_case_id}")
       break
