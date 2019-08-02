a7491c4be7b2feae73b8d32321d957b0462196cd
Stephen MacVicar <smacvicar@mitre.org>
2019-08-02 10:55:56 -0400

    add more unhappy path tests

--- test/integration/oauth2_endpoints_test.rb
+++ test/integration/oauth2_endpoints_test.rb
@@ -78,6 +78,53 @@ class OAuth2EndpointsTest < MiniTest::Test
     end
   end
 
+  def test_launch_response_no_iss
+    instance = create_testing_instance(url: 'http://example.com/no_iss')
+    sequence_result = create_sequence_result(
+      testing_instance: instance,
+      wait_at_endpoint: 'launch',
+      redirect_to_url: '/redirect'
+    )
+    Inferno::Models::TestResult.create(
+      sequence_result: sequence_result
+    )
+
+    EventMachine.run do
+      cookies = { 'HTTP_COOKIE' => "instance_id_test_set=#{instance.id}/" }
+      get '/inferno/oauth2/static/launch', {}, cookies
+
+      assert last_response.ok?
+
+      redirect_path = "#{Inferno::BASE_PATH}/#{instance.id}/test_sets/#{sequence_result.test_set_id}/#SMARTonFHIRTesting/#{sequence_result.test_case_id}"
+      assert last_response.body.include? js_redirect(redirect_path)
+
+      assert sequence_result.test_results.any? do |result|
+        result.fail? && result.message == 'No iss for redirect'
+      end
+      break
+    end
+  end
+
+  def test_launch_response_not_waiting
+    instance = create_testing_instance
+    sequence_result = create_sequence_result(
+      testing_instance: instance,
+      result: 'pass'
+    )
+    Inferno::Models::TestResult.create(
+      sequence_result: sequence_result
+    )
+
+    EventMachine.run do
+      cookies = { 'HTTP_COOKIE' => "instance_id_test_set=#{instance.id}/" }
+      get '/inferno/oauth2/static/launch?iss=http://example.com', {}, cookies
+
+      assert last_response.redirect?
+      assert last_response.headers['Location'].include? '?error=no_ehr_launch'
+      break
+    end
+  end
+
   def test_redirect_response_success
     instance = create_testing_instance(state: 'abc123')
     sequence_result = create_sequence_result(
@@ -110,6 +157,52 @@ class OAuth2EndpointsTest < MiniTest::Test
 
       expected_error_message = "No actively running launch sequences found with a state of #{bad_state}"
       assert last_response.body.include? expected_error_message
+      break
+    end
+  end
+
+  def test_redirect_response_bad_state
+    instance = create_testing_instance(state: '123')
+    sequence_result = create_sequence_result(
+      testing_instance: instance,
+      wait_at_endpoint: 'redirect'
+    )
+    Inferno::Models::TestResult.create(
+      sequence_result: sequence_result
+    )
+
+    EventMachine.run do
+      cookies = { 'HTTP_COOKIE' => "instance_id_test_set=#{instance.id}/" }
+      get '/inferno/oauth2/static/redirect?state=abc', {}, cookies
+
+      assert last_response.ok?
+
+      redirect_path = "#{Inferno::BASE_PATH}/#{instance.id}/test_sets/#{sequence_result.test_set_id}/#SMARTonFHIRTesting/#{sequence_result.test_case_id}"
+      assert last_response.body.include? js_redirect(redirect_path)
+
+      assert sequence_result.test_results.any? do |result|
+        result.fail? && result.message.start_with?('State provided in redirect')
+      end
+      break
+    end
+  end
+
+  def test_redirect_response_not_waiting
+    instance = create_testing_instance(state: 'abc123')
+    sequence_result = create_sequence_result(
+      testing_instance: instance,
+      result: 'pass'
+    )
+    Inferno::Models::TestResult.create(
+      sequence_result: sequence_result
+    )
+
+    EventMachine.run do
+      cookies = { 'HTTP_COOKIE' => "instance_id_test_set=#{instance.id}/" }
+      get '/inferno/oauth2/static/redirect?state=xyz', {}, cookies
+
+      assert last_response.redirect?
+      assert last_response.headers['Location'].include? '?error=no_state'
       break
     end
   end
