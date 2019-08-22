179a986f20b0efdf40aa2e28f15e4abaf281f83a
Yunwei Wang <yunwwang@gmail.com>
2019-08-22 14:06:53 -0500

    Add bulk data files

---
+++ lib/app/modules/bulk_data/bulk_data_patient_export_sequence.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+module Inferno
+  module Sequence
+    class BulkDataPatientExportSequence < SequenceBase
+      group 'Bulk Data Patient Export'
+
+      title 'Patient Tests'
+
+      description 'Verify that Patient resources on the Bulk Data server follow the Bulk Data Access Implementation Guide'
+
+      test_id_prefix 'Patient'
+
+      requires :token
+      conformance_supports :Patient
+
+      def content_location
+        @@content_location
+      end
+
+      # export
+      def export_kick_off
+        headers = { accept: 'application/fhir+json', prefer: 'respond-async' }
+
+        url = '/Patient/$export'
+
+        reply = @client.get(url, @client.fhir_headers(headers))
+        reply
+      end
+
+      details %(
+
+        The #{title} Sequence tests `#{title}` operations.  The operation steps will be checked for consistency against the
+        [Bulk Data Access Implementation Guide](https://build.fhir.org/ig/HL7/bulk-data/)
+
+      )
+
+      @resources_found = false
+
+      test 'Server rejects Patient search without authorization' do
+        metadata do
+          id '01'
+          link 'http://www.fhir.org/guides/argonaut/r2/Conformance-server.html'
+          desc %(
+          )
+          versions :r4
+        end
+
+        @client.set_no_auth
+        skip 'Could not verify this functionality when bearer token is not set' if @instance.token.blank?
+      end
+
+      test 'Server returns "202 Accepted" and "Cotent-location" for $export operation' do
+        metadata do
+          id '02'
+          link 'https://build.fhir.org/ig/HL7/US-Core-R4/CapabilityStatement-us-core-server.html'
+          desc %(
+          )
+          versions :r4
+        end
+
+        reply = export_kick_off
+
+        assert_response_accepted(reply)
+
+        @content_location = reply.response[:headers]['content-location']
+
+        assert !@content_location.empty?, 'Server must include Cotent-Location header for $export request'
+      end
+    end
+  end
+end
