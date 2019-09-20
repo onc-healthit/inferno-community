7ffb73abecac1c0a8a485efe19646627cd238cd3
Rob Scanlon <robscanlon@gmail.com>
2019-09-20 14:47:21 -0400

    Refactor Generator.

---
+++ generator/generator_base.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+require 'erb'
+require 'pry'
+require 'fileutils'
+require 'net/http'
+require 'fhir_models'
+
+module Inferno
+  module Generator
+    class Base
+      def initialize(path, extras)
+        @path = path
+        @extras = extras
+        load_resources
+      end
+
+      def load_resources
+        @resource_by_path = Hash.new {}
+        @resources_by_type = Hash.new { |h, k| h[k] = [] }
+        Dir.glob("#{resource_file_path}/**/*.*") do |resource| # note one extra "*"
+          if File.file?(resource) && (resource.end_with?('json') || resource.end_with?('xml'))
+
+            # We should consider using the native Ruby models instead of JSON
+            # There were problems with round-tripping certain SearchParameters though
+            new_resource_json = JSON.parse(File.read(resource))
+            new_resource = FHIR.from_contents(File.read(resource))
+            @resource_by_path[resource_path(new_resource)] = new_resource_json
+            type = new_resource.class.name.demodulize
+            type = 'CapabilityStatement' if type == 'Conformance'
+            @resources_by_type[type].push(new_resource_json)
+          end
+        end
+      end
+
+      def ig_resource
+        @resources_by_type['ImplementationGuide'].first
+      end
+
+      def capability_statement(mode)
+        @resources_by_type['CapabilityStatement'].find { |re| re['rest'].any? { |r| r['mode'] == mode } }
+      end
+
+      def resource_path(resource)
+        "#{resource.class.name.demodulize}/#{resource.id}"
+      end
+
+      def format_output
+        # system('sh', "rubocop -x --display-only-fail-level-offenses #{sequence_out_path}/")
+        system("rubocop -x --display-only-fail-level-offenses #{sequence_out_path}")
+      end
+
+      def run
+        generate
+        format_output
+      end
+
+      # subclass must implement the following:
+      # def generate
+
+      # end
+
+      def sequence_prefix
+        version = ig_resource['version'].delete('.')
+        name = ig_metadata['name']
+        "#{version}#{name}"
+      end
+
+      def module_yml_out_path
+        File.expand_path('./lib/app/modules/')
+      end
+
+      def sequence_out_path
+        File.expand_path("./lib/app/modules/#{@path}")
+      end
+
+      def resource_file_path
+        File.expand_path("./resources/#{@path}")
+      end
+    end
+  end
+end
---
+++ generator/uscore/metadata_extractor.rb
@@ -0,0 +1,251 @@
+# frozen_string_literal: true
+
+module Inferno
+  module Generator
+    module USCoreMetadataExtractor
+      PROFILE_URIS = Inferno::ValidationUtil::US_CORE_R4_URIS
+
+      def profile_uri(profile)
+        "http://hl7.org/fhir/us/core/StructureDefinition/#{profile}"
+      end
+
+      def profile_json_uri(profile)
+        "https://www.hl7.org/fhir/us/core/StructureDefinition-#{profile}.json"
+      end
+
+      def search_param_path(resource, param)
+        param = 'id' if param == '_id'
+        "SearchParameter/us-core-#{resource.downcase}-#{param}"
+      end
+
+      def extract_metadata
+        metadata = {
+          # The 'key' for the module is just the directory the IG is in
+          name: @path
+        }
+
+        # Note: consider using the Ruby representation instead of JSON
+        # We aren't right now because some information is being scrubbed
+        # from SearchParameter resource
+
+        capability_statement_json = capability_statement('server')
+        add_metadata_from_ig(metadata, ig_resource)
+        add_metadata_from_resources(metadata, capability_statement_json['rest'][0]['resource'])
+        add_special_cases(metadata)
+      end
+
+      def add_metadata_from_ig(metadata, implementation_guide)
+        metadata[:title] = "#{implementation_guide['title']} v#{implementation_guide['version']}"
+        metadata[:description] = "#{implementation_guide['title']} v#{implementation_guide['version']}"
+      end
+
+      def build_new_sequence(resource, profile)
+        base_path = profile.split('us/core/').last
+        base_name = profile.split('StructureDefinition/').last
+        profile_json = @resource_by_path[base_path]
+        reformatted_version = ig_resource['version'].delete('.')
+        profile_title = profile_json['title'].gsub(/US\s*Core\s*/, '').gsub(/\s*Profile/, '').strip
+        class_name = base_name
+          .split('-')
+          .map(&:capitalize)
+          .join
+          .gsub('UsCore', "USCore#{reformatted_version}") + 'Sequence'
+
+        # In case the profile doesn't start with US Core
+        class_name = "USCore#{reformatted_version}#{class_name}" unless class_name.start_with? 'USCore'
+
+        {
+          name: base_name.tr('-', '_'),
+          class_name: class_name,
+          resource: resource['type'],
+          profile: profile_uri(base_name), # link in capability statement is incorrect,
+          profile_json: profile_json_uri(base_name),
+          title: profile_title,
+          interactions: [],
+          searches: [],
+          search_param_descriptions: {},
+          element_descriptions: {},
+          must_supports: [],
+          tests: []
+        }
+      end
+
+      def add_metadata_from_resources(metadata, resources)
+        metadata[:sequences] = []
+
+        resources.each do |resource|
+          # This doesn't get ValueSet, which doesn't have a profile
+          # because it is to check ValueSet expansion
+          # We should update this to get that
+          resource['supportedProfile']&.each do |supported_profile|
+            new_sequence = build_new_sequence(resource, supported_profile)
+
+            add_basic_searches(resource, new_sequence)
+            add_combo_searches(resource, new_sequence)
+            add_interactions(resource, new_sequence)
+
+            base_path = new_sequence[:profile].split('us/core/').last
+            profile_definition = @resource_by_path[base_path]
+            add_must_support_elements(profile_definition, new_sequence)
+            add_search_param_descriptions(profile_definition, new_sequence)
+            add_element_definitions(profile_definition, new_sequence)
+
+            metadata[:sequences] << new_sequence
+          end
+        end
+      end
+
+      def add_basic_searches(resource, sequence)
+        basic_searches = resource['searchParam']
+        basic_searches&.each do |search_param|
+          new_search_param = {
+            names: [search_param['name']],
+            expectation: search_param['extension'][0]['valueCode']
+          }
+          sequence[:searches] << new_search_param
+          sequence[:search_param_descriptions][search_param['name'].to_sym] = {}
+        end
+      end
+
+      def add_combo_searches(resource, sequence)
+        search_combos = resource['extension'] || []
+        search_combo_url = 'http://hl7.org/fhir/StructureDefinition/capabilitystatement-search-parameter-combination'
+        search_combos
+          .select { |combo| combo['url'] == search_combo_url }
+          .each do |combo|
+            combo_params = combo['extension']
+            new_search_combo = {
+              expectation: combo_params[0]['valueCode'],
+              names: []
+            }
+            combo_params.each do |param|
+              next unless param.key?('valueString')
+
+              new_search_combo[:names] << param['valueString']
+              sequence[:search_param_descriptions][param['valueString'].to_sym] = {}
+            end
+            sequence[:searches] << new_search_combo
+          end
+      end
+
+      def add_interactions(resource, sequence)
+        interactions = resource['interaction']
+        interactions&.each do |interaction|
+          new_interaction = {
+            code: interaction['code'],
+            expectation: interaction['extension'][0]['valueCode']
+          }
+          sequence[:interactions] << new_interaction
+        end
+      end
+
+      def add_must_support_elements(profile_definition, sequence)
+        profile_definition['snapshot']['element'].select { |el| el['mustSupport'] }.each do |element|
+          if element['path'].end_with? 'extension'
+            sequence[:must_supports] <<
+              {
+                type: 'extension',
+                id: element['id'],
+                path: element['path'],
+                url: element['type'].first['profile'].first
+              }
+            next
+          end
+
+          path = element['path']
+          if path.include? '[x]'
+            choice_el = profile_definition['snapshot']['element'].find { |el| el['id'] == (path.split('[x]').first + '[x]') }
+            choice_el['type'].each do |type|
+              sequence[:must_supports] <<
+                {
+                  type: 'element',
+                  path: path.gsub('[x]', type['code'].slice(0).capitalize + type['code'].slice(1..-1))
+                }
+            end
+          else
+            sequence[:must_supports] <<
+              {
+                type: 'element',
+                path: path
+              }
+          end
+        end
+      end
+
+      def add_search_param_descriptions(profile_definition, sequence)
+        sequence[:search_param_descriptions].each_key do |param|
+          search_param_definition = @resource_by_path[search_param_path(sequence[:resource], param.to_s)]
+          path_parts = search_param_definition['xpath'].split('/f:')
+          if param.to_s != '_id'
+            path_parts[0] = sequence[:resource]
+            path = path_parts.join('.')
+          else
+            path = path_parts[0]
+          end
+          profile_element = profile_definition['snapshot']['element'].select { |el| el['id'] == path }.first
+          param_metadata = {
+            path: path,
+            comparators: {}
+          }
+          if !profile_element.nil?
+            param_metadata[:type] = profile_element['type'].first['code']
+            param_metadata[:contains_multiple] = (profile_element['max'] == '*')
+          else
+            # search is a variable type eg.) Condition.onsetDateTime - element in profile def is Condition.onset[x]
+            param_metadata[:type] = search_param_definition['type']
+            param_metadata[:contains_multiple] = false
+          end
+          search_param_definition['comparator']&.each_with_index do |comparator, index|
+            expectation_extension = search_param_definition['_comparator']
+            expectation = 'MAY'
+            expectation = expectation_extension[index]['extension'].first['valueCode'] unless expectation_extension.nil?
+            param_metadata[:comparators][comparator.to_sym] = expectation
+          end
+          sequence[:search_param_descriptions][param] = param_metadata
+        end
+      end
+
+      def add_element_definitions(profile_definition, sequence)
+        profile_definition['snapshot']['element'].each do |element|
+          next if element['type'].nil? # base profile
+
+          path = element['id']
+          if path.include? '[x]'
+            element['type'].each do |type|
+              sequence[:element_descriptions][path.gsub('[x]', type['code']).downcase.to_sym] = { type: type['code'], contains_multiple: element['max'] == '*' }
+            end
+          else
+            sequence[:element_descriptions][path.downcase.to_sym] = { type: element['type'].first['code'], contains_multiple: element['max'] == '*' }
+          end
+        end
+      end
+
+      def add_special_cases(metadata)
+        category_first_profiles = [
+          PROFILE_URIS[:diagnostic_report_lab],
+          PROFILE_URIS[:lab_results],
+          PROFILE_URIS[:diagnostic_report_note]
+        ]
+
+        # search by patient first
+        metadata[:sequences].each do |sequence|
+          patient_search = sequence[:searches].select { |param| param[:names] == ['patient'] } &.first
+          unless patient_search.nil?
+            sequence[:searches].delete(patient_search)
+            sequence[:searches].unshift(patient_search)
+          end
+        end
+
+        # search by patient + category first for these specific profiles
+        metadata[:sequences].select { |sequence| category_first_profiles.include?(sequence[:profile]) }.each do |sequence|
+          category_search = sequence[:searches].select { |param| param[:names] == ['patient', 'category'] } &.first
+          unless category_search.nil?
+            sequence[:searches].delete(category_search)
+            sequence[:searches].unshift(category_search)
+          end
+        end
+        metadata
+      end
+    end
+  end
+end
