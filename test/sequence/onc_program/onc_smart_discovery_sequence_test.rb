50def74bfbc0d5faa73efc84f8eba45ccee79e9f
Stephen MacVicar <Jammjammjamm@users.noreply.github.com>
2019-09-26 08:56:10 -0400

    fix smart discovery sequence name
    Co-Authored-By: Rob Scanlon <robscanlon@gmail.com>

--- test/sequence/onc_program/onc_smart_discovery_sequence_test.rb
+++ test/sequence/onc_program/onc_smart_discovery_sequence_test.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../../test_helper'
-class ONCProgramProvenanceTest < MiniTest::Test
+class ONCSMARTDiscoveryTest < MiniTest::Test
   def setup
     instance = get_test_instance
     client = get_client(instance)
