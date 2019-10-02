ff1a7144c6bffac9a16464b1b4848d989d6159b0
Rob Scanlon <robscanlon@gmail.com>
2019-10-01 22:40:54 -0400

    Add better error messaging in duplicate test_id check.

--- test/unit/module_validation_test.rb
+++ test/unit/module_validation_test.rb
@@ -11,11 +11,14 @@ class ModuleValidationTest < MiniTest::Test
   # Within any module, multiple sequences with the same test_id should not ever be loaded.
   # Note that we currently allow duplicates if the same sequence is referenced multiple times.
   def test_no_duplicate_test_ids
+    errors = []
     @modules&.each do |inferno_module|
       unique_sequences = inferno_module.sequences.uniq
       test_ids = unique_sequences.flat_map(&:tests).map { |test| test[:test_id] }
       duplicate_id = test_ids.detect { |t| test_ids.count(t) > 1 }
-      assert duplicate_id.nil?, "Found at least one duplicated test_id in module #{inferno_module.name}: #{duplicate_id}."
+      errors << "#{inferno_module.name} (#{duplicate_id})" unless duplicate_id.nil?
     end
+
+    assert errors.empty?, "Found at least one duplicate test_id in the following modules: #{errors.join(', ')}"
   end
 end
