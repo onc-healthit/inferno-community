af4865db865e929aea8a2559a32aa76ab9cd988b
Stephen MacVicar <smacvicar@mitre.org>
2019-10-21 15:55:12 -0400

    WIP track assertions in helper methods

--- test/support/sequence_coverage_reporting.rb
+++ test/support/sequence_coverage_reporting.rb
@@ -30,7 +30,7 @@ class SequenceProcessor
   end
 
   def stripped_file_name
-    @stripped_file_name ||= file_name.split('lib/app/modules/').last
+    @stripped_file_name ||= file_name.split('lib/app/').last
   end
 end
 
@@ -38,13 +38,14 @@ end
 # assertion is called
 class AssertionTracker
   class << self
+
     def assertion_method_names
       @assertion_method_names ||= Set.new(Inferno::Assertions.instance_methods)
     end
 
     # Assertion locations determined through analyzing the AST
     def assertion_locations
-      @assertion_locations ||= []
+      @assertion_locations ||= Set.new
     end
 
     # Assertion calls detected at runtime
@@ -60,9 +61,65 @@ class AssertionTracker
       assertion_locations << location
     end
 
-    def add_assertion_call(location, result)
+    def add_assertion_call(result)
+      location = AssertionCallLocationFormatter.new.location
+      return if location.blank?
+
+      add_assertion_location(location)
       assertion_calls[location] << result
     end
+  end
+end
+
+class AssertionCallLocationFormatter
+  SEQUENCE_LINE_REGEX = %r{inferno/lib/app/(modules/\w+/\w+\.rb:\d+)}
+  LINE_REGEX = %r{inferno/lib/app/((?:\w+/?)+\.rb:\d+)}
+  ASSERTION_CALL_REGEX = %r{inferno/lib/app/utils/assertions.rb:\d+}
+
+  attr_accessor :sequence_call_index, :assertion_call_index
+
+  def initialize
+    generate_sequence_call_index
+    generate_assertion_call_index
+  end
+
+  def backtrace
+    @backtrace ||= caller_locations
+  end
+
+  def generate_sequence_call_index
+    self.sequence_call_index = backtrace.rindex { |location| location.to_s.match? SEQUENCE_LINE_REGEX }
+
+    adjust_index_to_handle_blocks
+  end
+
+  def generate_assertion_call_index
+    self.assertion_call_index = backtrace.rindex { |location| location.to_s.match? ASSERTION_CALL_REGEX }
+  end
+
+  def method_name
+    @method_name ||= backtrace[sequence_call_index].to_s.match(/`(.*)'/)&.[](1)
+  end
+
+  # This adjusts sequence_call_index so that when assertions are wrapped in a
+  # warning block, this index refers to the method call inside the warning block
+  # rather than the warning call itself
+  def adjust_index_to_handle_blocks
+    return if sequence_call_index.blank? || assertion_call_index.blank?
+
+    (assertion_call_index...sequence_call_index).to_a.reverse.each do |index|
+      if backtrace[index].to_s.match?(/block .*in #{method_name}/)
+        self.sequence_call_index = index
+      end
+    end
+  end
+
+  def location
+    return unless sequence_call_index.present? && assertion_call_index.present?
+    [backtrace[sequence_call_index], backtrace[assertion_call_index + 1]]
+      .uniq
+      .map { |location| location&.to_s&.match(LINE_REGEX)&.[](1) }
+      .join(' -> ')
   end
 end
 
@@ -70,39 +127,27 @@ class AssertionReporter
   class << self
     def report
       create_csv
-      print_unknown_call_sites
     end
 
     def create_csv
       CSV.open(File.join(__dir__, '..', '..', 'sequence_coverage.csv'), 'wb') do |csv|
         csv << ['Assertion Location', 'Pass Count', 'Fail Count']
         AssertionTracker.assertion_locations.sort.each do |location|
-          results = AssertionTracker.assertion_calls[location]
-          csv << [location, pass_count(results), fail_count(results)]
+          csv << [location, pass_count(location), fail_count(location)]
         end
       end
     end
 
-    def pass_count(results)
-      results.count { |result| result }
+    def pass_count(location)
+      AssertionTracker.assertion_calls.reduce(0) do |count, (key, results)|
+        key.end_with?(location) ? count + results.count { |result| result } : count
+      end
     end
 
-    def fail_count(results)
-      results.count(&:!)
-    end
-
-    # Locations which called an assertion which was not detected in the AST.
-    # This happens when a method from outside of the sequence makes an assertion
-    # (e.g., a method from SequenceBase).
-    def unknown_call_sites
-      AssertionTracker.assertion_calls.keys - AssertionTracker.assertion_locations
-    end
-
-    def print_unknown_call_sites
-      return if unknown_call_sites.blank?
-
-      puts "\nUnrecognized assertion call sites:"
-      puts unknown_call_sites.join("\n")
+    def fail_count(location)
+      AssertionTracker.assertion_calls.reduce(0) do |count, (key, results)|
+        key.end_with?(location) ? count + results.count(&:!) : count
+      end
     end
   end
 end
