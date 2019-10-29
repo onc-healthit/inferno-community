b4ee6a5afc7380f4f4537bb88e1d7a294967d3b1
Stephen MacVicar <smacvicar@mitre.org>
2019-10-29 11:23:26 -0400

    refactor onc launch sequences

--- test/sequence/onc_program/onc_ehr_launch_sequence_test.rb
+++ test/sequence/onc_program/onc_ehr_launch_sequence_test.rb
@@ -7,7 +7,6 @@ describe Inferno::Sequence::OncEHRLaunchSequence do
     @sequence_class = Inferno::Sequence::OncEHRLaunchSequence
     @client = FHIR::Client.new('http://www.example.com/fhir')
     @instance = Inferno::Models::TestingInstance.new
-    @required_scopes = ['openid', 'fhirUser', 'launch', 'offline_access']
   end
 
   describe 'ONC scopes test' do
@@ -17,17 +16,17 @@ describe Inferno::Sequence::OncEHRLaunchSequence do
     end
 
     it 'fails when a required scope is missing' do
-      @required_scopes.each do |scope|
-        scopes = @required_scopes - [scope]
+      @sequence.required_scopes.each do |scope|
+        scopes = @sequence.required_scopes - [scope]
         @instance.instance_variable_set(:'@scopes', scopes.join(' '))
         exception = assert_raises(Inferno::AssertionException) { @sequence.run_test(@test) }
 
-        assert_equal %(Scope did not include "#{scope}"), exception.message
+        assert_equal "Required scopes missing: #{scope}", exception.message
       end
     end
 
     it 'fails when there is no user-level scope' do
-      @instance.instance_variable_set(:'@scopes', @required_scopes.join(' '))
+      @instance.instance_variable_set(:'@scopes', @sequence.required_scopes.join(' '))
       exception = assert_raises(Inferno::AssertionException) { @sequence.run_test(@test) }
 
       assert_equal 'Must contain a user-level scope in the format: user/[ resource | * ].[ read | *].', exception.message
@@ -36,21 +35,21 @@ describe Inferno::Sequence::OncEHRLaunchSequence do
     it 'fails when there is a badly formatted scope' do
       bad_scopes = ['user/*/*', 'patient/*.read', 'user/*.*.*', 'user/*.write']
       bad_scopes.each do |scope|
-        @instance.instance_variable_set(:'@scopes', (@required_scopes + [scope]).join(' '))
+        @instance.instance_variable_set(:'@scopes', (@sequence.required_scopes + [scope]).join(' '))
         exception = assert_raises(Inferno::AssertionException) { @sequence.run_test(@test) }
 
         assert_equal "Scope '#{scope}' does not follow the format: user/[ resource | * ].[ read | * ]", exception.message
       end
 
       bad_resource_type = 'ValueSet'
-      @instance.instance_variable_set(:'@scopes', (@required_scopes + ["user/#{bad_resource_type}.*"]).join(' '))
+      @instance.instance_variable_set(:'@scopes', (@sequence.required_scopes + ["user/#{bad_resource_type}.*"]).join(' '))
       exception = assert_raises(Inferno::AssertionException) { @sequence.run_test(@test) }
 
       assert_equal "'#{bad_resource_type}' must be either a valid resource type or '*'", exception.message
     end
 
     it 'succeeds when the required scopes and a user-level scope are present' do
-      @instance.instance_variable_set(:'@scopes', (@required_scopes + ['user/*.*']).join(' '))
+      @instance.instance_variable_set(:'@scopes', (@sequence.required_scopes + ['user/*.*']).join(' '))
 
       @sequence.run_test(@test)
     end
--- test/sequence/onc_program/onc_standalone_launch_sequence_test.rb
+++ test/sequence/onc_program/onc_standalone_launch_sequence_test.rb
@@ -7,7 +7,6 @@ describe Inferno::Sequence::OncStandaloneLaunchSequence do
     @sequence_class = Inferno::Sequence::OncStandaloneLaunchSequence
     @client = FHIR::Client.new('http://www.example.com/fhir')
     @instance = Inferno::Models::TestingInstance.new
-    @required_scopes = ['openid', 'fhirUser', 'launch/patient', 'offline_access']
   end
 
   describe 'ONC scopes test' do
@@ -17,17 +16,17 @@ describe Inferno::Sequence::OncStandaloneLaunchSequence do
     end
 
     it 'fails when a required scope is missing' do
-      @required_scopes.each do |scope|
-        scopes = @required_scopes - [scope]
+      @sequence.required_scopes.each do |scope|
+        scopes = @sequence.required_scopes - [scope]
         @instance.instance_variable_set(:'@scopes', scopes.join(' '))
         exception = assert_raises(Inferno::AssertionException) { @sequence.run_test(@test) }
 
-        assert_equal %(Scope did not include "#{scope}"), exception.message
+        assert_equal "Required scopes missing: #{scope}", exception.message
       end
     end
 
     it 'fails when there is no patient-level scope' do
-      @instance.instance_variable_set(:'@scopes', @required_scopes.join(' '))
+      @instance.instance_variable_set(:'@scopes', @sequence.required_scopes.join(' '))
       exception = assert_raises(Inferno::AssertionException) { @sequence.run_test(@test) }
 
       assert_equal 'Must contain a patient-level scope in the format: patient/[ resource | * ].[ read | *].', exception.message
@@ -36,21 +35,21 @@ describe Inferno::Sequence::OncStandaloneLaunchSequence do
     it 'fails when there is a badly formatted scope' do
       bad_scopes = ['patient/*/*', 'user/*.read', 'patient/*.*.*', 'patient/*.write']
       bad_scopes.each do |scope|
-        @instance.instance_variable_set(:'@scopes', (@required_scopes + [scope]).join(' '))
+        @instance.instance_variable_set(:'@scopes', (@sequence.required_scopes + [scope]).join(' '))
         exception = assert_raises(Inferno::AssertionException) { @sequence.run_test(@test) }
 
         assert_equal "Scope '#{scope}' does not follow the format: patient/[ resource | * ].[ read | * ]", exception.message
       end
 
       bad_resource_type = 'ValueSet'
-      @instance.instance_variable_set(:'@scopes', (@required_scopes + ["patient/#{bad_resource_type}.*"]).join(' '))
+      @instance.instance_variable_set(:'@scopes', (@sequence.required_scopes + ["patient/#{bad_resource_type}.*"]).join(' '))
       exception = assert_raises(Inferno::AssertionException) { @sequence.run_test(@test) }
 
       assert_equal "'#{bad_resource_type}' must be either a valid resource type or '*'", exception.message
     end
 
     it 'succeeds when the required scopes and a patient-level scope are present' do
-      @instance.instance_variable_set(:'@scopes', (@required_scopes + ['patient/*.*']).join(' '))
+      @instance.instance_variable_set(:'@scopes', (@sequence.required_scopes + ['patient/*.*']).join(' '))
 
       @sequence.run_test(@test)
     end
