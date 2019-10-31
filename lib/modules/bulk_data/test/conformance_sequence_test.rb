8a1edd575746f68fbc949faaa38c850a197398ed
Stephen MacVicar <smacvicar@mitre.org>
2019-10-31 13:11:17 -0400

    move bulk_data tests

--- lib/modules/bulk_data/test/conformance_sequence_test.rb
+++ lib/modules/bulk_data/test/conformance_sequence_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative '../../test_helper'
+require_relative '../../../../test/test_helper'
 
 class BulkDataCapabilityStatementSequenceTest < MiniTest::Test
   def setup
