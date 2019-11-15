3308838fd6ac479942e953c9c74d406de2fb8a1a
Stephen MacVicar <smacvicar@mitre.org>
2019-11-15 11:47:48 -0500

    refactor read tests to use validate_read_reply instead of fetch_resource

--- generator/uscore/uscore_generator.rb
+++ generator/uscore/uscore_generator.rb
@@ -115,7 +115,10 @@ module Inferno
         read_test[:test_code] = %(
               #{sequence[:resource].downcase}_id = @instance.resource_references.find { |reference| reference.resource_type == '#{sequence[:resource]}' }&.resource_id
               skip 'No #{sequence[:resource]} references found from the prior searches' if #{sequence[:resource].downcase}_id.nil?
-              @#{sequence[:resource].downcase} = fetch_resource('#{sequence[:resource]}', #{sequence[:resource].downcase}_id)
+              @#{sequence[:resource].downcase} = validate_read_reply(
+                FHIR::#{sequence[:resource]}.new(id: #{sequence[:resource].downcase}_id),
+                FHIR::#{sequence[:resource]}
+              )
               @#{sequence[:resource].downcase}_ary = Array.wrap(@#{sequence[:resource].downcase})
               @resources_found = !@#{sequence[:resource].downcase}.nil?)
         sequence[:tests] << read_test
