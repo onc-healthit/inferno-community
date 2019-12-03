2e84d89c162457bf08fb9412a68dd159acf9ff24
Rob Scanlon <robscanlon@gmail.com>
2019-12-03 16:28:39 -0500

    Move prefix to the test-case level.

--- lib/app/models/module/test_case.rb
+++ lib/app/models/module/test_case.rb
@@ -4,30 +4,28 @@ module Inferno
   class Module
     class TestCase
       attr_accessor :id
-      attr_accessor :prefix
-      attr_accessor :test_group
-      attr_accessor :sequence
       attr_accessor :parameters
+      attr_accessor :sequence
+      attr_accessor :test_group
 
-      def initialize(id, test_group, sequence, index, parameters)
+      def initialize(id, test_group, sequence, parameters)
         @id = id
         @sequence = sequence
-        @test_group = test_group
         @parameters = parameters
-
-        return unless test_group.prefix.present?
-
-        index_as_string = 'A'.dup
-        index.times { index_as_string.next! }
-        @prefix = "#{test_group.prefix}#{index_as_string}-"
-      end
-
-      def title
-        parameters[:title] || sequence.title
+        @test_group = test_group
       end
 
       def description
         parameters[:description] || sequence.description
+      end
+
+      def prefix
+        return unless parameters.has_key?(:prefix)
+        "#{parameters[:prefix]}-"
+      end
+
+      def title
+        parameters[:title] || sequence.title
       end
 
       def variable_defaults
--- lib/app/models/module/test_group.rb
+++ lib/app/models/module/test_group.rb
@@ -10,12 +10,11 @@ module Inferno
       attr_accessor :lock_variables
       attr_accessor :name
       attr_accessor :overview
-      attr_accessor :prefix
+      attr_accessor :test_cases
+      attr_accessor :test_set
       attr_accessor :run_all
       attr_accessor :run_skipped
       attr_accessor :test_case_names
-      attr_accessor :test_cases
-      attr_accessor :test_set
       attr_accessor :tags
 
       def initialize(test_set, group)
@@ -29,27 +28,26 @@ module Inferno
         @input_instructions = group[:input_instructions]
         @lock_variables = group[:lock_variables] || []
         @run_skipped = group[:run_skipped] || false
-        @prefix = group[:prefix]
         @tags = group[:tags]&.map do |tag|
           Tag.new(tag[:name], tag[:description], tag[:url])
         end || []
 
-        group[:sequences].each_with_index do |sequence, index|
+        group[:sequences].each do |sequence|
           if sequence.instance_of?(String)
-            add_test_case(sequence, index + 1)
+            add_test_case(sequence)
           else
-            add_test_case(sequence[:sequence], index + 1, sequence)
+            add_test_case(sequence[:sequence], sequence)
           end
         end
       end
 
-      def add_test_case(sequence_name, index, parameters = {})
+      def add_test_case(sequence_name, parameters = {})
         current_name = "#{test_set.id}_#{id}_#{sequence_name}"
-        count = 1
+        index = 1
         while test_case_names.include? current_name
-          count += 1
-          current_name = "#{test_set.id}_#{id}_#{sequence_name}_#{count}"
-          raise 'Too many test cases using the same scenario' if count > 99
+          index += 1
+          current_name = "#{test_set.id}_#{id}_#{sequence_name}_#{index}"
+          raise 'Too many test cases using the same scenario' if index > 99
         end
 
         test_case_names << current_name
@@ -58,7 +56,7 @@ module Inferno
 
         raise "No such sequence: #{sequence_name}" if sequence.nil?
 
-        new_test_case = TestCase.new(current_name, self, sequence, index, parameters)
+        new_test_case = TestCase.new(current_name, self, sequence, parameters)
 
         test_cases << new_test_case
 
