b9a512e7f16189e0987a127c2c8cae734da62e49
Rob Scanlon <robscanlon@gmail.com>
2019-12-10 16:22:10 -0500

    Regenerate tests.

--- lib/modules/uscore_v3.1.0/bodyheight_sequence.rb
+++ lib/modules/uscore_v3.1.0/bodyheight_sequence.rb
@@ -16,26 +16,24 @@ module Inferno
         case property
 
         when 'status'
-          value_found = can_resolve_path(resource, 'status') { |value_in_resource| value_in_resource == value }
-          assert value_found, 'status on resource does not match status requested'
+          value_found = resolve_element_from_path(resource, 'status') { |value_in_resource| value.split(',').include? value_in_resource }
+          assert value_found.present?, 'status on resource does not match status requested'
 
         when 'category'
-          value_found = can_resolve_path(resource, 'category.coding.code') { |value_in_resource| value_in_resource == value }
-          assert value_found, 'category on resource does not match category requested'
+          value_found = resolve_element_from_path(resource, 'category.coding.code') { |value_in_resource| value.split(',').include? value_in_resource }
+          assert value_found.present?, 'category on resource does not match category requested'
 
         when 'code'
-          value_found = can_resolve_path(resource, 'code.coding.code') { |value_in_resource| value_in_resource == value }
-          assert value_found, 'code on resource does not match code requested'
+          value_found = resolve_element_from_path(resource, 'code.coding.code') { |value_in_resource| value.split(',').include? value_in_resource }
+          assert value_found.present?, 'code on resource does not match code requested'
 
         when 'date'
-          value_found = can_resolve_path(resource, 'effective') do |date|
-            validate_date_search(value, date)
-          end
-          assert value_found, 'date on resource does not match date requested'
+          value_found = resolve_element_from_path(resource, 'effective') { |date| validate_date_search(value, date) }
+          assert value_found.present?, 'date on resource does not match date requested'
 
         when 'patient'
-          value_found = can_resolve_path(resource, 'subject.reference') { |reference| [value, 'Patient/' + value].include? reference }
-          assert value_found, 'patient on resource does not match patient requested'
+          value_found = resolve_element_from_path(resource, 'subject.reference') { |reference| [value, 'Patient/' + value].include? reference }
+          assert value_found.present?, 'patient on resource does not match patient requested'
 
         end
       end
@@ -85,6 +83,8 @@ module Inferno
           versions :r4
         end
 
+        @observation_ary = []
+
         code_val = ['8302-2']
         code_val.each do |val|
           search_params = { 'patient': @instance.patient_id, 'code': val }
@@ -92,13 +92,13 @@ module Inferno
           assert_response_ok(reply)
           assert_bundle_response(reply)
 
-          @resources_found = reply&.resource&.entry&.any? { |entry| entry&.resource&.resourceType == 'Observation' }
-          next unless @resources_found
+          next unless reply&.resource&.entry&.any? { |entry| entry&.resource&.resourceType == 'Observation' }
 
+          @resources_found = true
           @observation = reply.resource.entry
             .find { |entry| entry&.resource&.resourceType == 'Observation' }
             .resource
-          @observation_ary = fetch_all_bundled_resources(reply.resource)
+          @observation_ary += fetch_all_bundled_resources(reply.resource)
 
           save_resource_ids_in_bundle(versioned_resource_class('Observation'), reply, Inferno::ValidationUtil::US_CORE_R4_URIS[:body_height])
           save_delayed_sequence_references(@observation_ary)
@@ -133,6 +133,7 @@ module Inferno
 
         reply = get_resource_by_params(versioned_resource_class('Observation'), search_params)
         validate_search_reply(versioned_resource_class('Observation'), reply, search_params)
+        assert_response_ok(reply)
 
         ['gt', 'lt', 'le', 'ge'].each do |comparator|
           comparator_val = date_comparator_value(comparator, search_params[:date])
@@ -165,6 +166,7 @@ module Inferno
 
         reply = get_resource_by_params(versioned_resource_class('Observation'), search_params)
         validate_search_reply(versioned_resource_class('Observation'), reply, search_params)
+        assert_response_ok(reply)
       end
 
       test :search_by_patient_code_date do
@@ -193,6 +195,7 @@ module Inferno
 
         reply = get_resource_by_params(versioned_resource_class('Observation'), search_params)
         validate_search_reply(versioned_resource_class('Observation'), reply, search_params)
+        assert_response_ok(reply)
 
         ['gt', 'lt', 'le', 'ge'].each do |comparator|
           comparator_val = date_comparator_value(comparator, search_params[:date])
@@ -227,6 +230,7 @@ module Inferno
 
         reply = get_resource_by_params(versioned_resource_class('Observation'), search_params)
         validate_search_reply(versioned_resource_class('Observation'), reply, search_params)
+        assert_response_ok(reply)
       end
 
       test :read_interaction do
@@ -436,7 +440,7 @@ module Inferno
         must_support_elements.each do |path|
           @observation_ary&.each do |resource|
             truncated_path = path.gsub('Observation.', '')
-            must_support_confirmed[path] = true if can_resolve_path(resource, truncated_path)
+            must_support_confirmed[path] = true if resolve_element_from_path(resource, truncated_path).present?
             break if must_support_confirmed[path]
           end
           resource_count = @observation_ary.length
