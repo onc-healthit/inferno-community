44a00f79b53f8c61d3ac55f81ad777cf8d1f7036
Chase <czhou@mitre.org>
2020-01-21 12:03:36 -0500

    remove reference logic since it's not being used

--- lib/app/sequence_base.rb
+++ lib/app/sequence_base.rb
@@ -527,19 +527,14 @@ module Inferno
       def validate_read_reply(resource, klass)
         class_name = klass.name.demodulize
         assert !resource.nil?, "No #{class_name} resources available from search."
-        resource_is_reference = resource.is_a? FHIR::DSTU2::Reference
-        if resource_is_reference
-          read_response = resource.read
-        else
-          id = resource.try(:id)
-          assert !id.nil?, "#{class_name} id not returned"
-          read_response = @client.read(klass, id)
-          assert_response_ok read_response
-          read_response = read_response.resource
-        end
+        id = resource&.id
+        assert !id.nil?, "#{class_name} id not returned"
+        read_response = @client.read(klass, id)
+        assert_response_ok read_response
+        read_response = read_response.resource
         assert !read_response.nil?, "Expected #{class_name} resource to be present."
         assert read_response.is_a?(klass), "Expected resource to be of type #{class_name}."
-        assert read_response.id == id, "Expected resource to contain id: #{id}" unless resource_is_reference
+        assert read_response.id == id, "Expected resource to contain id: #{id}"
         read_response
       end
 
