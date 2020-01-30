37859037b71cb7535bd2b6b7923d2c71f9ad3884
Stephen MacVicar <smacvicar@mitre.org>
2020-01-30 13:55:19 -0500

    make dar code checking more rigorous

--- generator/uscore/static/data_absent_reason_checker.rb
+++ generator/uscore/static/data_absent_reason_checker.rb
@@ -2,6 +2,9 @@
 
 module Inferno
   module DataAbsentReasonChecker
+    DAR_EXTENSION_URL = 'http://hl7.org/fhir/StructureDefinition/data-absent-reason'
+    DAR_CODE_SYSTEM_URL = 'http://terminology.hl7.org/CodeSystem/data-absent-reason'
+
     def check_for_data_absent_reasons
       proc do |reply|
         if !@instance.data_absent_extension_found && contains_data_absent_extension?(reply.body)
@@ -17,11 +20,20 @@ module Inferno
     end
 
     def contains_data_absent_extension?(body)
-      body.include? 'http://hl7.org/fhir/StructureDefinition/data-absent-reason'
+      body.include? DAR_EXTENSION_URL
     end
 
     def contains_data_absent_code?(body)
-      body.include? 'http://terminology.hl7.org/CodeSystem/data-absent-reason'
+      if body.include? DAR_CODE_SYSTEM_URL
+        resource = FHIR.from_contents(body)
+        walk_resource(resource) do |element, meta, path|
+          next unless meta['type'] == 'Coding'
+
+          return true if element.code == 'unknown' && element.system == DAR_CODE_SYSTEM_URL
+        end
+      end
+
+      false
     end
   end
 end
