e798e6b05a1cea36df7395fded8123ae90d67f37
Hershil Patel <hershil@mitre.org>
2020-02-07 18:34:58 -0500

    updated mcode sequence to reflect new changes in inferno

--- lib/modules/mcode/mcode_primary_cancer_condition_sequence.rb
+++ lib/modules/mcode/mcode_primary_cancer_condition_sequence.rb
@@ -12,16 +12,16 @@ module Inferno
 		requires :token, :patient_id
 		conformance_supports :Condition
 
-		  def validate_resource_item(resource, property, value)
+	      def validate_resource_item(resource, property, value)
 	        case property
 
 	        when 'patient'
-	          value_found = can_resolve_path(resource, 'subject.reference') { |reference| [value, 'Patient/' + value].include? reference }
-	          assert value_found, 'patient on resource does not match patient requested'
+	          value_found = resolve_element_from_path(resource, 'subject.reference') { |reference| [value, 'Patient/' + value].include? reference }
+	          assert value_found.present?, 'patient on resource does not match patient requested'
 
 	        when 'type'
-	          value_found = can_resolve_path(resource, 'type.coding.code') { |value_in_resource| value_in_resource == value }
-	          assert value_found, 'type on resource does not match type requested'
+	          value_found = resolve_element_from_path(resource, 'type.coding.code') { |value_in_resource| value.split(',').include? value_in_resource }
+	          assert value_found.present?, 'type on resource does not match type requested'
 
 	        end
 	      end
@@ -30,40 +30,38 @@ module Inferno
 			The #{title} Sequence tests `#{title.gsub(/\s+/, '')}` resources associated with the provided patient.
 		  )
 
-		  @resources_found = false
-
-		  
+	      @resources_found = false
 
 		  test :unauthorized_search do
-			metadata do
-			  id '01'
-			  name 'Server rejects Primary Cancer Condition search without authorization'
-			  #link 'https://www.hl7.org/fhir/us/core/CapabilityStatement-us-core-server.html#behavior'
-			  link 'http://build.fhir.org/ig/HL7/fhir-mCODE-ig/branches/master/StructureDefinition-onco-core-PrimaryCancerCondition.html'
-			  description %(
-				A server SHALL reject any unauthorized requests by returning an HTTP 401 unauthorized response code.
-			  )
-			  versions :r4
-			end
+	        metadata do
+	          id '01'
+	          name 'Server rejects Condition search without authorization'
+	          link 'https://www.hl7.org/fhir/us/core/CapabilityStatement-us-core-server.html#behavior'
+	          description %(
+	            A server SHALL reject any unauthorized requests by returning an HTTP 401 unauthorized response code.
+	          )
+	          versions :r4
+	        end
 
-			#skip_if_not_supported(:PrimaryCancerCondition, [:search])
+	        skip_if_known_not_supported(:Condition, [:search])
 
-			@client.set_no_auth
-			omit 'Do not test if no bearer token set' if @instance.token.blank?
+	        @client.set_no_auth
+	        omit 'Do not test if no bearer token set' if @instance.token.blank?
 
-			search_params = {
-			  'patient': @instance.patient_id
-			}
+	        search_params = {
+	        	'patient': @instance.patient_id
+	        }
 
-			reply = get_resource_by_params('PrimaryCancerCondition', search_params)
-			@client.set_bearer_token(@instance.token)
-			assert_response_unauthorized reply
-		  end
+	        reply = get_resource_by_params(versioned_resource_class('Condition'), search_params)
+	        @client.set_bearer_token(@instance.token)
+	        assert_response_unauthorized reply
+	      end
 
-		  test 'Server returns expected results from Condition search by patient' do
+		  test :search_by_patient do
 	        metadata do
 	          id '02'
-	          link 'http://build.fhir.org/ig/HL7/fhir-mCODE-ig/branches/master/StructureDefinition-onco-core-PrimaryCancerCondition.html'
+	          name 'Server returns expected results from Condition search by patient'
+	          link 'https://www.hl7.org/fhir/us/core/CapabilityStatement-us-core-server.html'
 	          description %(
 
 	            A server SHALL support searching by patient on the Condition resource
@@ -72,31 +70,27 @@ module Inferno
 	          versions :r4
 	        end
 
-	        #need to dynamically determine this profile based on inputed endpoint
-	        profile = "https://api.logicahealth.org/mCODEstu1/StructureDefinition/onco-core-PrimaryCancerCondition"
-
-	        search_params = {
-	          'patient': @instance.patient_id,
-	          '_profile': profile
+			search_params = {
+	           'patient': @instance.patient_id
 	        }
-
-	        
 
 	        reply = get_resource_by_params(versioned_resource_class('Condition'), search_params)
 	        assert_response_ok(reply)
 	        assert_bundle_response(reply)
 
-	        resource_count = reply&.resource&.entry&.length || 0
-	        @resources_found = true if resource_count.positive?
+        	@resources_found = reply&.resource&.entry&.any? { |entry| entry&.resource&.resourceType == 'Condition' }
 
-	        skip 'No resources appear to be available for this patient. Please use patients with more information.' unless @resources_found
+        	skip 'No Condition resources appear to be available. Please use patients with more information.' unless @resources_found
 
-	        @condition = reply&.resource&.entry&.first&.resource
-	        @condition_ary = fetch_all_bundled_resources(reply&.resource)
+	        @condition = reply.resource.entry
+	          .find { |entry| entry&.resource&.resourceType == 'Condition' }
+	          .resource
+	        @condition_ary = fetch_all_bundled_resources(reply.resource)
 	        save_resource_ids_in_bundle(versioned_resource_class('Condition'), reply)
 	        save_delayed_sequence_references(@condition_ary)
-	        validate_search_reply(versioned_resource_class('Condition'), reply, search_params)
-          end
+	        validate_search_reply(versioned_resource_class('Condition'), reply, search_params) #causing issues, doesn't seem to be finished 
+
+      	  end
 
           test 'Server returns expected results from Condition by id' do
 	        metadata do
@@ -128,19 +122,13 @@ module Inferno
 	          versions :r4
 	        end
 
-	        puts '-----------------------------------------------------------'
-
+	        #need to use this profile because this is what exists in mcode structure definitions json
 	        profile = "http://hl7.org/fhir/us/mcode/StructureDefinition/onco-core-PrimaryCancerCondition"
 
 	        skip 'No resources appear to be available for this patient. Please use patients with more information.' unless @resources_found
 	        test_resources_against_profile('Condition', profile)
 
-	        puts '-----------------------------------------------------------'
-
-	      end
-
-
-          
+	      end          
 		end
 	  end
 	end
