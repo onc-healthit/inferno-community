bc08a0a6af3462d49081d8945211c8a7d6fe53a8
Luke Osborne <lwosborne@mitre.org>
2020-02-13 14:26:18 -0500

    Update valueset sequence tests for r4

--- test/sequence/quality_reporting/valueset_sequence_test.rb
+++ test/sequence/quality_reporting/valueset_sequence_test.rb
@@ -15,30 +15,41 @@ class ValueSetSequenceTest < MiniTest::Test
   CQF_REQUEST_HEADERS = {
     'Accept' => '*/*',
     'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-    'Host' => 'localhost:8080'
+    'Host' => 'localhost:8080',
+    'User-Agent' => 'rest-client/2.1.0 (darwin18.7.0 x86_64) ruby/2.5.6p201'
   }.freeze
 
-  # load up the CMS130 'measure-col' bundle to use for testing
+  # load up the CMS130 artifacts to use for testing
   PROJECT_ROOT = "#{__dir__}/../../.."
-  MEASURE_COL_BUNDLE_PATH = File.expand_path('resources/quality_reporting/CMS130/Bundle/cms130-bundle.json', PROJECT_ROOT)
-  MEASURE_COL_BUNDLE = FHIR::STU3::Bundle.new JSON.parse(File.read(MEASURE_COL_BUNDLE_PATH))
+  EXM_130_MEASURE_PATH = File.expand_path('test/fixtures/measure-EXM130_FHIR4-7.2.000.json', PROJECT_ROOT)
+  EXM_130_DEPENDENT_LIBRARY_BUNDLE_PATH = File.expand_path('test/fixtures/library-deps-EXM130_FHIR4-7.2.000-bundle.json', PROJECT_ROOT)
+  EXM_130_MAIN_LIBRARY_PATH = File.expand_path('test/fixtures/library-EXM130_FHIR4-7.2.000.json', PROJECT_ROOT)
+  EXM_130_MEASURE = FHIR::Measure.new JSON.parse(File.read(EXM_130_MEASURE_PATH))
+  EXM_130_MAIN_LIBRARY = FHIR::Library.new JSON.parse(File.read(EXM_130_MAIN_LIBRARY_PATH))
+  EXM_130_DEPENDENT_LIBRARY_BUNDLE = FHIR::Bundle.new JSON.parse(File.read(EXM_130_DEPENDENT_LIBRARY_BUNDLE_PATH))
 
   MEASURES_TO_TEST = [
     {
-      measure_id: 'MitreTestScript-measure-col',
-      bundle: MEASURE_COL_BUNDLE
+      measure: EXM_130_MEASURE,
+      main_library: EXM_130_MAIN_LIBRARY,
+      dependent_library_bundle: EXM_130_DEPENDENT_LIBRARY_BUNDLE
     }
   ].freeze
 
   def stub_measure_resource_requests(measure_info)
-    measure_resource = measure_info[:bundle].entry.select { |e| e.resource.id == 'MitreTestScript-measure-col' }.first.resource
-    stub_request(:get, "http://localhost:8080/cqf-ruler-dstu3/fhir/Measure/#{measure_info[:measure_id]}")
+    measure_resource = measure_info[:measure]
+
+    stub_request(:get, "http://localhost:8080/cqf-ruler-r4/fhir/Measure/#{measure_info[:measure].id}")
       .with(headers: CQF_REQUEST_HEADERS)
       .to_return(status: 200, body: measure_resource.to_json, headers: {})
 
-    library_resources = measure_info[:bundle].entry.select { |e| e.resource.resourceType == 'Library' }.map(&:resource)
+    stub_request(:get, "http://localhost:8080/cqf-ruler-r4/fhir/Library/#{measure_info[:main_library].id}")
+      .with(headers: CQF_REQUEST_HEADERS)
+      .to_return(status: 200, body: measure_info[:main_library].to_json, headers: {})
+
+    library_resources = measure_info[:dependent_library_bundle]
     library_resources.each do |library|
-      stub_request(:get, "http://localhost:8080/cqf-ruler-dstu3/fhir/Library/#{library.id}")
+      stub_request(:get, "http://localhost:8080/cqf-ruler-r4/fhir/Library/#{library.id}")
         .with(headers: CQF_REQUEST_HEADERS)
         .to_return(status: 200, body: library.to_json, headers: {})
     end
@@ -48,7 +59,7 @@ class ValueSetSequenceTest < MiniTest::Test
     @instance = Inferno::Models::TestingInstance.new(url: 'http://www.example.com', selected_module: 'quality_reporting')
     @instance.save!
     client = FHIR::Client.new(@instance.url)
-    client.use_stu3
+    client.use_r4
     client.default_json
     @sequence = Inferno::Sequence::ValueSetSequence.new(@instance, client, true)
   end
@@ -63,7 +74,7 @@ class ValueSetSequenceTest < MiniTest::Test
 
       stub_measure_resource_requests(measure_info)
 
-      @instance.measure_to_test = measure_info[:measure_id]
+      @instance.measure_to_test = measure_info[:measure].id
       sequence_result = @sequence.start
       assert sequence_result.pass?, 'The sequence should be marked as pass.'
       assert sequence_result.test_results.all? { |r| r.pass? || r.skip? }, 'All tests should pass'
@@ -80,7 +91,7 @@ class ValueSetSequenceTest < MiniTest::Test
 
       stub_measure_resource_requests(measure_info)
 
-      @instance.measure_to_test = measure_info[:measure_id]
+      @instance.measure_to_test = measure_info[:measure].id
       sequence_result = @sequence.start
       assert sequence_result.fail?, 'The sequence should be marked as fail.'
     end
