0dbd29ba4546e87bef90c20928f88463ba722060
Jason Crowley <crowleyj@mitre.org>
2020-07-20 11:53:15 -0400

    Add methods for loading IGs into the validator by ID and by package.tgz

--- lib/app/utils/hl7_validator.rb
+++ lib/app/utils/hl7_validator.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'json'
+
 module Inferno
   # A validator that calls out to the HL7 validator API
   class HL7Validator
@@ -28,6 +30,26 @@ module Inferno
         warnings: warnings,
         information: information
       }
+    end
+
+    # @param [String] id the NPM package ID of the IG to be loaded into the validator
+    # @return [Array<String>] a list of profiles belonging to the loaded IG
+    def load_ig_by_id(id)
+      Inferno.logger.info("Loading IG with id #{id} from packages.fhir.org")
+      Inferno.logger.info("PUT #{@validator_url}/igs/#{id}")
+
+      result = RestClient.put "#{@validator_url}/igs/#{id}", {}
+      JSON.parse(result.body)
+    end
+
+    # @param [String] package_tgz the package.tgz contents of the IG to be loaded into the validator
+    # @return [Array<String>] a list of profiles belonging to the loaded IG
+    def load_ig_by_tgz(package_tgz)
+      Inferno.logger.info('Loading custom IG from a package.tgz')
+      Inferno.logger.info("POST #{@validator_url}/igs")
+
+      result = RestClient.post "#{@validator_url}/igs", package_tgz, content_encoding: 'gzip'
+      JSON.parse(result.body)
     end
 
     private
