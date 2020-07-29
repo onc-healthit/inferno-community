4da2b53dd5de28278df6dc0b8a52c08c56f06e41
Chase <czhou@mitre.org>
2020-07-29 12:39:43 -0400

    saving some progress

--- generator/generic/generic_generator.rb
+++ generator/generic/generic_generator.rb
@@ -2,21 +2,28 @@
 
 require_relative '../generator_base'
 require_relative '../sequence_metadata'
+require_relative '../search_parameter_metadata'
 require_relative './read_test'
 require_relative './profile_validation_test'
+require_relative './search_test'
 
 module Inferno
   module Generator
     class GenericGenerator < Generator::Base
       include ReadTest
       include ProfileValidationTest
+      include SearchTest
 
       def resource_profiles
         resources_by_type['StructureDefinition'].reject { |definition| definition['type'] == 'Extension' }
       end
 
       def sequence_metadata
-        @sequence_metadata ||= resource_profiles.map { |profile| SequenceMetadata.new(profile) }
+        @sequence_metadata ||= resource_profiles.map { |profile| SequenceMetadata.new(profile, capability_statement) }
+      end
+
+      def search_parameter_metadata
+        @search_parameter_metadata ||= resources_by_type['SearchParameter'].map { |parameter_json| SearchParameterMetadata.new(parameter_json) }
       end
 
       def generate
@@ -28,6 +35,7 @@ module Inferno
         sequence_metadata.each do |metadata|
           create_read_test(metadata)
           create_profile_validation_test(metadata)
+          create_search_tests(metadata)
           generate_sequence(metadata)
         end
       end
@@ -38,6 +46,14 @@ module Inferno
         template = ERB.new(File.read(File.join(__dir__, 'templates/sequence.rb.erb')))
         output =   template.result_with_hash(metadata: metadata)
         FileUtils.mkdir_p(sequence_out_path + '/') unless File.directory?(sequence_out_path + '/')
+        File.write(file_name, output)
+      end
+
+      def generate_sequence_definitions(metadata)
+        file_name = sequence_out_path + '/profile_definitions/' + sequence[:name].downcase + '_definitions.rb'
+        template = ERB.new(File.read(File.join(__dir__, 'templates/sequence_definition.rb.erb')))
+        output = template.result_with_hash(metadata)
+        FileUtils.mkdir_p(sequence_out_path) unless File.directory?(sequence_out_path)
         File.write(file_name, output)
       end
 
---
+++ generator/search_parameter_metadata.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+module Inferno
+  module Generator
+    class SearchParameterMetadata
+      attr_reader :search_parameter_json
+      attr_writer :url,
+                  :code,
+                  :expression,
+                  :multiple_or,
+                  :multiple_or_expectation,
+                  :multiple_and,
+                  :multiple_and_expectation,
+                  :modifiers,
+                  :comparators
+
+      EXPECTATION_URL = 'http://hl7.org/fhir/StructureDefinition/capabilitystatement-expectation'
+      def initialize(search_parameter_json)
+        @search_parameter_json = search_parameter_json
+      end
+
+      def url
+        @url ||= @search_parameter_json['url']
+      end
+
+      def code
+        @code ||= @search_parameter_json['code']
+      end
+
+      def expression
+        @expression ||= @search_parameter_json['expression']
+      end
+
+      # whether multiple or is allowed
+      def multiple_or
+        @multiple_or ||= @search_parameter_json['multipleOr']
+      end
+
+      # expectation if multiple or is allowed - unsure if this is generic or just us core specific
+      def multiple_or_expectation
+        @multiple_or_expectation ||= @search_parameter_json['_multipleOr']['extension'].find { |ext| ext['url'] == EXPECTATION_URL }['valueCode']
+      end
+
+      # whether multiple and is allowed
+      def multiple_and
+        @multiple_and ||= @search_parameter_json['multipleAnd']
+      end
+
+      # expectation if multiple and is allowed - unsure if this is generic or just us core specific
+      def multiple_and_expectation
+        @multiple_and_expectation ||= @search_parameter_json['_multipleAnd']['extension'].find { |ext| ext['url'] == EXPECTATION_URL }['valueCode']
+      end
+
+      def comparators
+        return [] if @search_parameter_json['comparator'].nil?
+
+        @comparators ||= @search_parameter_json['comparator'].each_with_index.map do |comparator, index|
+          expectation_extension = @search_parameter_json['_comparator'] # unsure if this is us core specific
+          expectation = expectation_extension[index]['extension'].find { |ext| ext['url'] == EXPECTATION_URL }['valueCode'] unless expectation_extension.nil?
+          { comparator: comparator, expectation: expectation }
+        end
+      end
+
+      def modifiers
+        return [] if @search_parameter_json['modifier'].nil?
+
+        @modifiers ||= @search_parameter_json['modifier'].each_with_index.map do |modifier, index|
+          expectation_extension = @search_parameter_json['_modifier'] # unsure if this is us core specific
+          expectation = expectation_extension[index]['extension'].find { |ext| ext['url'] == EXPECTATION_URL }['valueCode'] unless expectation_extension.nil?
+          { comparator: modifier, expectation: expectation }
+        end
+      end
+    end
+  end
+end
