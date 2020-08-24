e861bb13526d1f682e54ee7950e3d2482367b917
Jason Crowley <crowleyj@mitre.org>
2020-08-24 14:47:47 -0400

    Make IndexBuilder a single-use class

--- lib/app/utils/index_builder.rb
+++ lib/app/utils/index_builder.rb
@@ -4,65 +4,50 @@ require 'json'
 
 module Inferno
   class IndexBuilder
-    @files = nil
-    @index = nil
 
-    # Prepares the IndexBuilder to start indexing files
-    def start
+    # @param folder [String] the path of the folder to index
+    def initialize(folder)
+      raise ArgumentError, "'#{folder}' is not a directory" unless Dir.exist?(folder)
+
+      @folder = folder
       @files = []
       @index = { 'index-version': 1, files: @files }
-      nil
     end
 
-    # Indexes the file with the given name and contents.
+    # @return [Boolean] whether or not the given folder has been indexed
+    def indexed?
+      File.exist?(File.join(@folder, '.index.json'))
+    end
+
+    # Builds and returns the string representing the contents of an .index.json file for the given folder.
+    #
+    # @return [String] a string representing the contents of an .index.json file
+    def build
+      Dir.chdir(@folder) do
+        Dir.glob('*.json').select { |f| File.file?(f) }.each do |filename|
+          add_json_file(filename, File.read(filename))
+        end
+      end
+      JSON.pretty_generate(@index)
+    end
+
+    private
+
+    # Adds a JSON file with the given name and contents to the index being built.
+    # Ignores files that don't contain a valid "resourceType" property.
+    #
     # @param filename [String] the name of the file to index
     # @param contents [String] the contents of the file to index
-    def see_file(filename, contents)
-      return unless filename.end_with?('.json')
+    def add_json_file(filename, contents)
+      file = JSON.parse(contents)
+      return unless file.is_a?(Hash)
+      return unless file['resourceType'].is_a?(String)
 
-      json = JSON.parse(contents)
-      return unless json.is_a?(Hash)
-      return unless (type = json['resourceType'])
-
-      file = { filename: filename, resourceType: type.to_s }
-      props = json.slice('id', 'url', 'version', 'kind', 'type', 'supplements')
-        .reject { |_, val| val.is_a?(Hash) || val.is_a?(Array) }
-        .transform_values { |val| val.is_a?(String) ? val : val.to_json }
-      @files << file.merge(props)
-
-      nil
-    rescue JSON::ParserError
-      Inferno.logger.error("Error parsing #{filename}: Invalid JSON")
-    end
-
-    # Builds and returns the string representing the contents of an .index.json file.
-    # @return [String] a string representing the contents of the .index.json file
-    def build
-      index = JSON.pretty_generate(@index)
-      @files = nil
-      @index = nil
-      index
-    end
-
-    # Indexes the folder at the given path <folder> or at <folder>/package if not already indexed.
-    # @param folder [String] the path of the folder to index
-    # @return [Boolean] whether a new .index.json was created
-    # @raise [StandardError] if there was an error reading a file
-    def execute(folder)
-      folder_package = File.join(folder, 'package')
-      (folder = folder_package) if Dir.exist?(folder_package)
-
-      Dir.chdir(folder) do
-        return false if File.exist?('.index.json')
-
-        start
-        Dir.glob('*.json').select { |f| File.file?(f) }.each do |filename|
-          see_file(filename, File.read(filename))
-        end
-        File.write('.index.json', build)
-      end
-
-      true
+      file.slice!('resourceType', 'id', 'url', 'version', 'kind', 'type', 'supplements')
+        .delete_if { |_, val| val.is_a?(Hash) || val.is_a?(Array) }
+        .transform_values! { |val| val.is_a?(String) ? val : val.to_json }
+      file['filename'] = filename
+      @files << file
     end
   end
 end
