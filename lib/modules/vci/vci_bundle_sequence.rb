fba9c635abd211b8b05e9b31e0a60ee191ff7486
Yunwei Wang <yunwwang@gmail.com>
2021-03-24 10:04:10 -0500

    Add VCI bundle sequence

--- lib/modules/vci/vci_bundle_sequence.rb
+++ lib/modules/vci/vci_bundle_sequence.rb
@@ -4,64 +4,56 @@ require_relative '../health_cards/shared_health_cards_tests'
 
 module Inferno
   module Sequence
-    class VciFileDownloadSequence < HealthCardsFileDownloadSequence
-      extends_sequence HealthCardsFileDownloadSequence
-      title 'Validates File Download against VCI profiles (FOR DEMO ONLY, REMOVE)'
+    class VciBundleSequence < SequenceBase
+      include Inferno::SequenceUtilities
 
-      test_id_prefix 'VCIFD'
+      title 'Validates VCI Bundles (FOR DEMO ONLY, REMOVE)'
+
+      test_id_prefix 'VCIB'
 
       requires :file_download_url
 
-      description 'VCI file download validation '
+      description 'VCI bundle validation '
 
       details %(
-        Validates jws content
+        Validates VCI Bundle
       )
 
-      def validate_bundles
-        bundle_index = 0
-        error_collection = []
+      requires :vci_bundle_json
 
-        @verifiable_credentials_bundles.each do |bundle|
-          errors = test_resource_against_profile(bundle, 'http://hl7.org/fhir/us/smarthealthcards-vaccination/StructureDefinition/vaccine-credential-bundle')
+      attr_accessor :vci_bundle
 
-          if errors.present?
-            errors.map! { |e| "Bundle[#{bundle_index}]: #{e}" }
-            error_collection << errors
-          end
 
-          bundle_index += 1
-        end
+      def validate_vci(resource_type, resource, profile_url)
+        errors = test_resource_against_profile(@vci_bundle, profile_url)
 
-        assert(error_collection.empty?, "\n* " + error_collection.join("\n* "))
+        errors.map! { |e| "#{resource_type}: #{e}" }
+
+        assert(errors.empty?, "\n* " + errors.join("\n* "))
       end
 
       def validate_vci_dm(resource_type, profile_url)
-        bundle_index = 0
         entry_index = 0
         error_collection = []
 
-        @verifiable_credentials_bundles.each do |bundle|
-          bundle.entry.each do |entry|
-            next unless entry.resource.class.name.demodulize == resource_type
+        @vci_bundle.entry.each do |entry|
+          next unless entry.resource.class.name.demodulize == resource_type
 
-            errors = test_resource_against_profile(entry.resource, profile_url)
+          errors = test_resource_against_profile(entry.resource, profile_url)
 
-            if errors.present?
-              errors.map! { |e| "Bundle[#{index}]/#{resource_type}/#{entry.fullUrl}: #{e}" }
-              error_collection << errors if errors.present?
-            end
-
-            entry_index += 1
+          if errors.present?
+            # TODO: use entry.fullUrl or index?
+            errors.map! { |e| "#{resource_type}/#{entry.fullUrl}: #{e}" }
+            error_collection << errors if errors.present?
           end
 
-          bundle_index += 1
+          entry_index += 1
         end
 
         assert(error_collection.empty?, "\n* " + error_collection.join("\n* "))
       end
 
-      def test_resource_against_profile(resource, profile_url)
+      def test_resource_against_profile(resource, profile_url)        
         resource_validation_errors = Inferno::RESOURCE_VALIDATOR.validate(resource, versioned_resource_class, profile_url)
 
         errors = resource_validation_errors[:errors]
@@ -83,9 +75,38 @@ module Inferno
           versions :r4
         end
 
-        skip 'No resource returned/provided' unless @verifiable_credentials_bundles.present?
+        skip 'No resource returned/provided' unless @vci_bundle.present? || @instance.vci_bundle_json.present?
 
-        validate_bundles
+        @vci_bundle = FHIR::Bundle.new(JSON.parse(@instance.vci_bundle_json)) if @vci_bundle.nil?
+
+        validate_vci('Bundle', @vci_bundle, 'http://hl7.org/fhir/us/smarthealthcards-vaccination/StructureDefinition/vaccine-credential-bundle')
+
+      end
+
+      test :resource_validate_patient do
+        metadata do
+          id '02'
+          name 'Patient resource in returned Bundle matches the Vaccine Credential Patient Allowable Data profiles'
+          link 'http://build.fhir.org/ig/dvci/vaccine-credential-ig/branches/main/StructureDefinition-vaccine-credential-patient'
+          description %(
+            This test will validate that the Patient resource in returned Bundle from the server matches the Vaccine Credential Patient Allowable Data profiles.
+          )
+          versions :r4
+        end
+        # FHIR Validator has a bug which could not return correct error message when the Patient resource is not correct in a Bundle.
+        # We add this test sequence to
+
+        skip 'No resource returned/provided' unless @vci_bundle.present?
+
+        resource_type = 'Patient'
+
+        entry = @vci_bundle.entry.select { |entry| entry.resource.class.name.demodulize == resource_type}.first
+
+        if (entry.present?)
+          validate_vci(resource_type, entry.resource, 'http://hl7.org/fhir/us/smarthealthcards-vaccination/StructureDefinition/vaccine-credential-patient')
+        else
+          assert false, 'VCI Bundle does NOT have VCI Patient entry'
+        end
       end
 
       test :resource_validate_patient_dm do
@@ -100,7 +121,7 @@ module Inferno
           optional
         end
 
-        skip 'No resource returned/provided' unless @verifiable_credentials_bundles.present?
+        skip 'No resource returned/provided' unless @vci_bundle.present?
 
         validate_vci_dm('Patient', 'http://hl7.org/fhir/us/smarthealthcards-vaccination/StructureDefinition/vaccine-credential-patient-dm')
       end
@@ -117,7 +138,7 @@ module Inferno
           optional
         end
 
-        skip 'No resource returned/provided' unless @verifiable_credentials_bundles.present?
+        skip 'No resource returned/provided' unless @vci_bundle.present?
 
         validate_vci_dm('Immunization', 'http://hl7.org/fhir/us/smarthealthcards-vaccination/StructureDefinition/vaccine-credential-immunization-dm')
       end
