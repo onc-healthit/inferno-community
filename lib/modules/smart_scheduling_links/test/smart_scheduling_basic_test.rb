db7b05857d8b5ff820a40aa27239e2a35ab48d34
Hershil Patel <hershil@mitre.org>
2021-04-07 20:37:03 -0400

    initial tests

--- lib/modules/smart_scheduling_links/test/smart_scheduling_basic_test.rb
+++ lib/modules/smart_scheduling_links/test/smart_scheduling_basic_test.rb
@@ -1,47 +1,371 @@
 # frozen_string_literal: true
 
 require_relative '../../../../test/test_helper'
+require 'json'
+
 
 describe Inferno::Sequence::SmartSchedulingLinksBasicSequence do
   before do
-    # @sequence_class = Inferno::Sequence::SMARTSchedulingBasic
-    # @base_url = 'http://www.example.com/fhir'
-    # @token = 'ABC'
-    # @instance = Inferno::TestingInstance.create(url: @base_url, token: @token, selected_module: 'ips')
-    # @client = FHIR::Client.for_testing_instance(@instance)
+    @sequence_class = Inferno::Sequence::SmartSchedulingLinksBasicSequence
+    @base_url = 'http://www.example.com/fhir'
+    @token = 'ABC'
+
+    @manifest_url = 'http://www.example.com/$bulk-manifest'
+    @locations = ['http://www.example.com/locations.ndjson']
+
+    @instance = Inferno::TestingInstance.create(url: @base_url, token: @token, selected_module: 'ips')   
+    
+
+    @client = FHIR::Client.for_testing_instance(@instance)
+
+
+    @sample_manifest_file = JSON.parse('{
+      "transactionTime" : "2021-03-10T18:16:34.535Z",
+      "request" : "http://www.example.com/$bulk-publish",
+      "output" : [
+        {
+          "type": "Location",
+          "url": "http://www.example.com/locations.ndjson"
+        },
+        {
+          "type": "Schedule",
+          "url": "http://www.example.com/schedules.ndjson"
+        },
+        {
+          "type": "Slot",
+          "url": "http://www.example.com/slots-2021-W09.ndjson",
+          "extension": {
+            "state": [
+              "MA"
+            ]
+          }
+        }
+      ]
+
+    
+    }')
+
     # @composition_resource = FHIR.from_contents(load_fixture(:composition))
     # @document_response = FHIR.from_contents(load_fixture(:document_response))
   end
 
-  # describe 'Document operation on composition' do
-  #   before do
-  #     @test = @sequence_class[:document_operator]
-  #     @sequence = @sequence_class.new(@instance, @client)
-  #     @sequence.instance_variable_set(:'@resource_found', @composition_resource)
-  #   end
 
-  #   it 'fails if search fails' do
-  #     stub_request(:get, "#{@base_url}/Composition/#{@composition_resource.id}/$document?persist=true")
-  #       .to_return(status: 401)
+  #01
+  describe 'manifest url is valid tests' do
+    before do
+      #@sequence = @sequence_class.new(@instance, @client)
+      @test = @sequence_class[:manifest_url_form]
+    end
 
-  #     exception = assert_raises(Inferno::AssertionException) { @sequence.run_test(@test) }
+    it 'fails when url does not end in /$bulk-publish' do
+      instance_copy = @instance.clone
+      instance_copy.manifest_url = 'http://sample-url/invalid'
+      @sequence = @sequence_class.new(instance_copy, @client)
 
-  #     assert_equal 'Bad response code: expected 200, 201, but found 401. ', exception.message
-  #   end
+      error = assert_raises(Inferno::AssertionException) do
+        @sequence.run_test(@test)
+      end
 
-  #   it 'fails if a referenced resource is missing' do
-  #     @document_response.entry.pop
-  #     stub_request(:get, "#{@base_url}/Composition/#{@composition_resource.id}/$document?persist=true")
-  #       .to_return(status: 200, body: @document_response.to_json)
+      assert_match("Manifest file must end in $bulk-publish", error.message)
 
-  #     assert_raises(Inferno::AssertionException) { @sequence.run_test(@test) }
-  #   end
+    end
 
-  #   it 'succeeds if all referenced resources are returned' do
-  #     stub_request(:get, "#{@base_url}/Composition/#{@composition_resource.id}/$document?persist=true")
-  #       .to_return(status: 200, body: @document_response.to_json)
+    it 'fails when manifest_url is not a url' do
+      instance_copy = @instance.clone
+      instance_copy.manifest_url = '$bulk-publish'
+      @sequence = @sequence_class.new(instance_copy, @client)
 
-  #     @sequence.run_test(@test)
-  #   end
-  # end
+      error = assert_raises(Inferno::AssertionException) do
+        @sequence.run_test(@test)
+      end
+
+      #assert_match("Manifest file must end in $bulk-publish", error.message)
+    end
+
+    it 'fails when manifest_url is empty' do
+      instance_copy = @instance.clone
+      instance_copy.manifest_url = ""
+      @sequence = @sequence_class.new(instance_copy, @client)
+
+      error = assert_raises(Inferno::AssertionException) do
+        @sequence.run_test(@test)
+      end
+
+      #assert_match("Manifest file must end in $bulk-publish", error.message)
+    end
+
+    it 'succeeds when url ends is $bulk-publish' do
+      instance_copy = @instance.clone
+      instance_copy.manifest_url = 'http://test.gov/$bulk-publish'
+      @sequence = @sequence_class.new(instance_copy, @client)
+
+      @sequence.run_test(@test)
+
+    end
+  end
+
+  #02
+  describe 'manifest file is valid json' do
+    before do
+      @test = @sequence_class[:manifest_downloadable]
+    end
+
+    it 'fails when manifest file is not valid json' do
+      invalid_json = 'invalid_json'
+      stub_request(:get, @manifest_url)
+        .to_return(status: 200, body: invalid_json, headers: {})
+
+      instance_copy = @instance.clone
+      instance_copy.manifest_url = @manifest_url 
+      @sequence = @sequence_class.new(instance_copy, @client)
+
+      error = assert_raises(Inferno::AssertionException) do
+        @sequence.run_test(@test)
+      end
+    end
+
+    it 'fails when manifest url is not a valid uri ' do
+      invalid_uri = 'invalid_uri'
+      stub_request(:get, invalid_uri)
+        .to_return(status: 200, body: "", headers: {})
+
+      instance_copy = @instance.clone
+      instance_copy.manifest_url = invalid_uri
+      @sequence = @sequence_class.new(instance_copy, @client)
+
+      error = assert_raises(Inferno::AssertionException) do
+        @sequence.run_test(@test)
+      end
+    end
+
+    it 'succeeds when manfest file is valid json' do
+      valid_json = '{"test" : "test"}'
+      stub_request(:get, @manifest_url)
+        .to_return(status: 200, body: valid_json, headers: {})
+
+      instance_copy = @instance.clone
+      instance_copy.manifest_url = @manifest_url 
+      @sequence = @sequence_class.new(instance_copy, @client)
+
+      @sequence.run_test(@test)
+    end
+  end
+
+  #03
+  describe 'manifest is structured properly' do
+    before do
+      @test = @sequence_class[:manifest_minimum_requirement]
+    end
+
+    it 'succeeds with manifest file containing each required field' do
+      instance_copy = @instance.clone
+      @sequence = @sequence_class.new(instance_copy, @client)
+      @sequence.instance_variable_set(:@manifest, @sample_manifest_file);
+      pass_exception = assert_raises(Inferno::PassException) do
+        @sequence.run_test(@test)
+      end
+    end
+    
+    it 'fails when missing transactionTime' do
+      instance_copy = @instance.clone
+      @sequence = @sequence_class.new(instance_copy, @client)
+      
+      sample_manifest_file_missing_transaction_time = JSON.parse('{
+        "request" : "http://www.example.com/$bulk-publish",
+        "output" : [
+          {
+            "type": "Location",
+            "url": "http://www.example.com/locations.ndjson"
+          },
+          {
+            "type": "Schedule",
+            "url": "http://www.example.com/schedules.ndjson"
+          },
+          {
+            "type": "Slot",
+            "url": "http://www.example.com/slots-2021-W09.ndjson",
+            "extension": {
+              "state": [
+                "MA"
+              ]
+            }
+          }
+        ]      
+      }')
+      
+      @sequence.instance_variable_set(:@manifest, sample_manifest_file_missing_transaction_time);
+
+      error = assert_raises(Inferno::AssertionException) do
+        @sequence.run_test(@test)
+      end
+    end
+  
+
+  
+    it 'fails when missing request' do
+      instance_copy = @instance.clone
+      @sequence = @sequence_class.new(instance_copy, @client)
+      
+      sample_manifest_file_missing_request = JSON.parse('{
+        "transactionTime" : "2021-03-10T18:16:34.535Z",
+        "output" : [
+          {
+            "type": "Location",
+            "url": "http://www.example.com/locations.ndjson"
+          },
+          {
+            "type": "Schedule",
+            "url": "http://www.example.com/schedules.ndjson"
+          },
+          {
+            "type": "Slot",
+            "url": "http://www.example.com/slots-2021-W09.ndjson",
+            "extension": {
+              "state": [
+                "MA"
+              ]
+            }
+          }
+        ]      
+      }')
+      
+      @sequence.instance_variable_set(:@manifest, sample_manifest_file_missing_request);
+
+      error = assert_raises(Inferno::AssertionException) do
+        @sequence.run_test(@test)
+      end
+    end
+
+    it 'fails when missing output' do
+      instance_copy = @instance.clone
+      @sequence = @sequence_class.new(instance_copy, @client)
+      
+      sample_manifest_file_missing_output = JSON.parse('{
+        "transactionTime" : "2021-03-10T18:16:34.535Z",
+        "request" : "http://www.example.com/$bulk-publish"  
+      }')
+      
+      @sequence.instance_variable_set(:@manifest, sample_manifest_file_missing_output);
+
+      error = assert_raises(Inferno::AssertionException) do
+        @sequence.run_test(@test)
+      end
+    end
+
+    
+  end
+
+  #04
+  describe 'manifest is structured properly' do
+    before do
+      @test = @sequence_class[:manifest_contains_jurisdictions]
+    end
+
+    it 'succeeds with manifest file containing each required field' do
+      instance_copy = @instance.clone
+      @sequence = @sequence_class.new(instance_copy, @client)
+      @sequence.instance_variable_set(:@manifest, @sample_manifest_file);
+      @sequence.run_test(@test)
+    end
+    
+    it 'fails when state is not an array' do
+      instance_copy = @instance.clone
+      @sequence = @sequence_class.new(instance_copy, @client)
+      
+      sample_manifest_file_state_not_array = JSON.parse('{
+        "request" : "http://www.example.com/$bulk-publish",
+        "output" : [
+          {
+            "type": "Location",
+            "url": "http://www.example.com/locations.ndjson"
+          },
+          {
+            "type": "Schedule",
+            "url": "http://www.example.com/schedules.ndjson"
+          },
+          {
+            "type": "Slot",
+            "url": "http://www.example.com/slots-2021-W09.ndjson",
+            "extension": {
+              "state" : "MA"
+            }
+          }
+        ]      
+      }')
+      
+      @sequence.instance_variable_set(:@manifest, sample_manifest_file_state_not_array);
+
+      error = assert_raises(Inferno::AssertionException) do
+        @sequence.run_test(@test)
+      end
+    end
+
+    it 'fails when state is not a 2 letter abbreviations' do
+      instance_copy = @instance.clone
+      @sequence = @sequence_class.new(instance_copy, @client)
+      
+      sample_manifest_file_state_not_2_letters = JSON.parse('{
+        "request" : "http://www.example.com/$bulk-publish",
+        "output" : [
+          {
+            "type": "Location",
+            "url": "http://www.example.com/locations.ndjson"
+          },
+          {
+            "type": "Schedule",
+            "url": "http://www.example.com/schedules.ndjson"
+          },
+          {
+            "type": "Slot",
+            "url": "http://www.example.com/slots-2021-W09.ndjson",
+            "extension": {
+              "state" : "MAA"
+            }
+          }
+        ]      
+      }')
+      
+      @sequence.instance_variable_set(:@manifest, sample_manifest_file_state_not_2_letters);
+
+      error = assert_raises(Inferno::AssertionException) do
+        @sequence.run_test(@test)
+      end
+    end
+  end
+
+  #05 - 07
+  describe 'manifest with since parameter tests' do
+    before do
+      @test = @sequence_class[:manifest_downloadable]
+    end
+
+    it 'succeeds when since parameter is ignored' do
+
+      
+    end
+
+    it 'succeeds when since parameter is implemented correctly' do
+
+    end
+
+    it 'fails when since parameter is not implement correctly' do
+    end
+  end
+
+  #10
+  describe 'location resources contain valid FHIR resources' do
+    before do
+      @test = @sequence_class[:location_valid]
+    end
+    it 'succeeds when location has valid FHIR resources' do
+      instance_copy = @instance.clone
+      @sequence = @sequence_class.new(instance_copy, @client)
+      @sequence.instance_variable_set(:@manifest, @sample_manifest_file);
+      @sequence.instance_variable_set(:@location_urls, @locations);
+
+      @sequence.run_test(@test)
+    end
+  end
+
 end
+
