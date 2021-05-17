8912391fd5c45c90f2e8807839dc1ec2ad2cbf3a
Yunwei Wang <yunwwang@gmail.com>
2021-05-17 11:55:29 -0500

    add ips bundle manual test

---
+++ lib/modules/ips_operation/ips_bundle_manual_sequence.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+Dir['lib/modules/ips/profile_definitions/*'].sort.each { |file| require './' + file }
+require_relative './ips_shared_bundle_tests'
+
+module Inferno
+  module Sequence
+    class IpsBundleManualSequence < SequenceBase
+      include Inferno::SequenceUtilities
+      include IpsProfileDefinitions
+      include SharedIpsBundleTests
+
+      title 'Bundle Manual Tests'
+      description 'Verify support for the server capabilities required by the Bundle (IPS) profile.'
+      details %(
+      )
+      test_id_prefix 'BUIM'
+      requires :ips_bundle_json
+
+      def run_test(test)
+        if (@bundle || []).empty?
+          @bundle = FHIR::Bundle.new(JSON.parse(@instance.ips_bundle_json))
+        end
+
+        super(test)
+      end
+
+      resource_validate_bundle(index: '01')
+      resource_validate_composition(index: '02')
+      resource_validate_medication_statement(index: '03')
+      resource_validate_allergy_intolerance(index: '04')
+      resource_validate_condition(index: '05')
+    end
+  end
+end
